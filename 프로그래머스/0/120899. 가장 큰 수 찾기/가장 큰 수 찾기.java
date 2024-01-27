4111240792644ecc80178a8288021109a18e83df
hyezuu <147456219+hyezuu@users.noreply.github.com>
2024-01-28 00:26:34 +0900

    [level 0] Title: 가장 큰 수 찾기, Time: 0.01 ms, Memory: 73.7 MB -BaekjoonHub

--- 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.java
+++ 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.java
@@ -1,11 +1,9 @@
 class Solution {
     public int[] solution(int[] array) {
         int[] answer = new int[2];
-        int max = 0;
         for(int i=0; i< array.length; i++){
-            if(array[i]>max){
-                max = array[i];
-                answer[0] = max;
+            if(array[i]>answer[0]){
+                answer[0] = array[i];
                 answer[1] = i;
             }
         }
