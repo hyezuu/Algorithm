cf9027b4be70da3dde7b7938f3fa9942a4dc785d
hyezuu <147456219+hyezuu@users.noreply.github.com>
2024-01-28 01:43:56 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 3.34 ms, Memory: 89.5 MB -BaekjoonHub

--- 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.java
+++ 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.java
@@ -4,7 +4,7 @@ class Solution {
     public int[] solution(String my_string) {
         return Arrays.stream(my_string.split(""))
             .filter(c -> Character.isDigit(c.charAt(0)))
-            .mapToInt(c -> Character.getNumericValue(c.charAt(0)))
+            .mapToInt(Integer::parseInt)
             .sorted()
             .toArray();
         
