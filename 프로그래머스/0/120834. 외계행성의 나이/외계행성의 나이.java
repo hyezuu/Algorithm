4fb414a22ee44e934da25906a0256d3a6827cf89
hyezuu <147456219+hyezuu@users.noreply.github.com>
2024-01-28 03:34:11 +0900

    [level 0] Title: 외계행성의 나이, Time: 1.61 ms, Memory: 75.7 MB -BaekjoonHub

--- 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.java
+++ 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.java
@@ -3,7 +3,7 @@ import java.util.stream.*;
 
 class Solution {
     public String solution(int age) {
-        String answer = String.valueOf(age);
+           String answer = String.valueOf(age);
         return Arrays.stream(answer.split(""))
             .map(c -> String.valueOf((char)(Character.getNumericValue(c.charAt(0))+('a'))))
             .collect(Collectors.joining());
