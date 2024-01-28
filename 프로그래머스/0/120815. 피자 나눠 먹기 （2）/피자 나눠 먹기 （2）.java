70696f307d96fa2eff318752896293fd4a73a82a
hyezuu <147456219+hyezuu@users.noreply.github.com>
2024-01-28 15:03:36 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.02 ms, Memory: 78.2 MB -BaekjoonHub

--- 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.java
+++ 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.java
@@ -1,7 +1,7 @@
 class Solution {
     public int solution(int n) {
         int answer = 0;
-        for(int i=1; i<=300; i++){
+        for(int i=1; i<=n; i++){
             if((i*6)%n ==0){
                 answer = i;
                 break;
