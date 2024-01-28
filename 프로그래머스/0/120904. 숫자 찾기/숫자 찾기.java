a400d9070856ecf403d7abc59dbab50f095c0dfc
hyezuu <147456219+hyezuu@users.noreply.github.com>
2024-01-28 15:14:36 +0900

    [level 0] Title: 숫자 찾기, Time: 5.91 ms, Memory: 76.6 MB -BaekjoonHub

--- 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.java
+++ 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.java
@@ -1,9 +1,6 @@
 class Solution {
     public int solution(int num, int k) {
-        String str = String.valueOf(num);
-        if(str.contains(String.valueOf(k))){
-            return str.indexOf(String.valueOf(k))+1;
-        }
-        else return -1;
+        return ("-" + num).indexOf(String.valueOf(k));
+        
     }
 }
