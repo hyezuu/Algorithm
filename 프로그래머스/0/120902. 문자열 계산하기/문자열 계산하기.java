56675eaa863ddc463b5e44b8815026540ed25210
hyezuu <147456219+hyezuu@users.noreply.github.com>
2024-01-30 00:12:16 +0900

    [level 0] Title: 문자열 계산하기, Time: 1.52 ms, Memory: 75.7 MB -BaekjoonHub

--- 프로그래머스/0/120902. 문자열 계산하기/문자열 계산하기.java
+++ 프로그래머스/0/120902. 문자열 계산하기/문자열 계산하기.java
@@ -2,16 +2,8 @@ import java.util.*;
 
 class Solution {
     public int solution(String my_string) {
-        String[] arr = my_string.split(" ");
-        int answer = Integer.parseInt(arr[0]);
-        for(int i=0; i<arr.length-1; i++){
-            if(arr[i].equals("+")){
-                answer += Integer.parseInt(arr[i+1]);
-            }
-            else if(arr[i].equals("-")){
-                answer -= Integer.parseInt(arr[i+1]);
-            }
-        }
-        return answer;
+        return Arrays.stream(my_string.replaceAll("- ","-").replaceAll("\\s[+]","").split(" "))
+            .mapToInt(Integer::parseInt)
+            .sum();
     }
 }
