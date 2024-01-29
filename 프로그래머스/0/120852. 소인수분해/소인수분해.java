f4ff1d408cb20cb6910993eb5bc52dfdd6cf9dcc
hyezuu <147456219+hyezuu@users.noreply.github.com>
2024-01-29 18:17:35 +0900

    [level 0] Title: 소인수분해, Time: 1.93 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/0/120852. 소인수분해/소인수분해.java
+++ 프로그래머스/0/120852. 소인수분해/소인수분해.java
@@ -2,20 +2,32 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int n) {
-        String str= "";
-        for(int i=2; i<=n; i++){
-            boolean isPrime= true;
+        
+        LinkedHashSet<Integer> primeNum = new LinkedHashSet<>();
+        int i = 2;
+        while(i<=n){
             if(n%i==0){
-                for(int j=2; j<=i; j++){
-                    if(i%j==0&&i!=j){
-                        isPrime= false;
-                    }
-                }
-                if(isPrime){
-                    str += i+" ";
-                }
-            }    
+                primeNum.add(i);
+                n/=i;
+            }
+            else i++;
         }
-        return Arrays.stream(str.split(" ")).mapToInt(Integer::parseInt).toArray();
+        return primeNum.stream().mapToInt(Integer::intValue).toArray();
+        
+        // String str= "";
+        // for(int i=2; i<=n; i++){
+        //     boolean isPrime= true;
+        //     if(n%i==0){
+        //         for(int j=2; j<=i; j++){
+        //             if(i%j==0&&i!=j){
+        //                 isPrime= false;
+        //             }
+        //         }
+        //         if(isPrime){
+        //             str += i+" ";
+        //         }
+        //     }    
+        // }
+        // return Arrays.stream(str.split(" ")).mapToInt(Integer::parseInt).toArray();
     }
 }
