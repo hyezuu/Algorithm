9924c4168694a5e9f90e8530c70f671758a81223
hyezuu <147456219+hyezuu@users.noreply.github.com>
2024-02-01 00:40:57 +0900

    [level 0] Title: 문자열 밀기, Time: 1.61 ms, Memory: 86.2 MB -BaekjoonHub

--- 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.java
+++ 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.java
@@ -1,9 +1,6 @@
 class Solution {
     public int solution(String A, String B) {
         int index = 0;
-        if(A.equals(B)){
-            return 0;
-        }
         String newB = B+B;
         index = newB.indexOf(A);
         if(index >= B.length()-1/2){
