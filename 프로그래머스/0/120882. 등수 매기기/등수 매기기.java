c86b40f0c66901169dd7ef1153afb33e6748d353
hyezuu <147456219+hyezuu@users.noreply.github.com>
2024-01-31 10:03:43 +0900

    [level 0] Title: 등수 매기기, Time: 10.89 ms, Memory: 83.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120882. 등수 매기기/등수 매기기.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[][] score) {
+        
+        double[] avg = Arrays.stream(score)
+            .mapToDouble(c -> Arrays.stream(c).average().orElse(0.0))
+            .toArray();
+        
+        double[] sortedAvg = Arrays.copyOf(avg, avg.length);
+        
+        Arrays.sort(sortedAvg);
+    
+        Map<Double, Integer> map = new HashMap<>();
+        
+        for(int i=0; i<sortedAvg.length; i++){
+            
+            if(i!=0 && sortedAvg[sortedAvg.length-i-1] == sortedAvg[sortedAvg.length-i]){
+                map.put(sortedAvg[sortedAvg.length-i-1],map.get(sortedAvg[sortedAvg.length-i]));
+            }
+            else map.put(sortedAvg[sortedAvg.length-i-1],i+1);
+        }
+          
+        return Arrays.stream(avg).mapToInt(map::get).toArray();
+    }
+}
