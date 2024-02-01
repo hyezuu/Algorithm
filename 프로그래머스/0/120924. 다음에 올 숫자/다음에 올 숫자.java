26b8823614c0e687d033702c3bc6798a660255fe
hyezuu <147456219+hyezuu@users.noreply.github.com>
2024-02-02 02:14:27 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.02 ms, Memory: 74.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int[] common) {
+        int comd1 = common[1]-common[0];
+        int comd2 = common[2]-common[1];
+        
+        
+        if(comd1==comd2){
+            return common[common.length-1] + comd1;
+        }
+        else return common[common.length-1] * (comd2/comd1);        
+    }
+}
