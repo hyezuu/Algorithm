70eaac551fa5759ee7cf4365a1bfa6322b209f3b
hyezuu <147456219+hyezuu@users.noreply.github.com>
2024-02-01 19:12:46 +0900

    [level 0] Title: 특이한 정렬, Time: 0.02 ms, Memory: 75.4 MB -BaekjoonHub

--- 프로그래머스/0/120880. 특이한 정렬/특이한 정렬.java
+++ 프로그래머스/0/120880. 특이한 정렬/특이한 정렬.java
@@ -4,16 +4,18 @@ import java.util.stream.*;
 
 class Solution {
     public int[] solution(int[] numlist, int n) {
-        int[] num1 = Arrays.stream(numlist).boxed()
-            .sorted(Comparator.reverseOrder())
-            .mapToInt(Integer::intValue)
-            .toArray();
+        // int[] num1 = Arrays.stream(numlist).boxed()
+        //     .sorted(Comparator.reverseOrder())
+        //     .mapToInt(Integer::intValue)
+        //     .toArray();
         //내림차순 정렬. 큰값이 앞에온다.
+        int[] num1 = numlist;
         
         for(int i=0; i<num1.length-1; i++){//num1.length-1 번 비교연산 수행한다.
             boolean isSwapped = false;
             for(int j=0; j<num1.length-1-i; j++){//현재값이랑 다음에 들어올 값중에 큰값들어오면 바꿔줄거임
-                if(Math.abs(num1[j]-n)>Math.abs(num1[j+1]-n)){
+                if(Math.abs(num1[j]-n)>Math.abs(num1[j+1]-n)
+                   ||(Math.abs(num1[j]-n)==Math.abs(num1[j+1]-n)&&num1[j]<num1[j+1])){
                     int temp = num1[j+1];
                     num1[j+1] = num1[j];
                     num1[j] = temp;
@@ -23,8 +25,7 @@ class Solution {
             if(isSwapped == false){
                 break;
             }
-        }
-                
+        }                
         return num1;
     }
 }
