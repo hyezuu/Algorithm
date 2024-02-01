195b1ce490a403cd2b106bc8f24c341937242183
hyezuu <147456219+hyezuu@users.noreply.github.com>
2024-02-02 02:14:43 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.02 ms, Memory: 73.7 MB -BaekjoonHub

--- 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.java
+++ 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.java
@@ -2,8 +2,7 @@ class Solution {
     public int solution(int[] common) {
         int comd1 = common[1]-common[0];
         int comd2 = common[2]-common[1];
-        
-        
+                
         if(comd1==comd2){
             return common[common.length-1] + comd1;
         }
