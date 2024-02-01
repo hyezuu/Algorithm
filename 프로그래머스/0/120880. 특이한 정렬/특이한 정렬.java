721d72dc7d88389bed1142524e06440ebea6bff6
hyezuu <147456219+hyezuu@users.noreply.github.com>
2024-02-01 19:04:42 +0900

    [level 0] Title: 특이한 정렬, Time: 2.85 ms, Memory: 76.1 MB -BaekjoonHub

--- 프로그래머스/0/120880. 특이한 정렬/특이한 정렬.java
+++ 프로그래머스/0/120880. 특이한 정렬/특이한 정렬.java
@@ -10,16 +10,23 @@ class Solution {
             .toArray();
         //내림차순 정렬. 큰값이 앞에온다.
         
-        for(int i=0; i<num1.length; i++){//num1.length-1 번 비교연산 수행한다.
+        for(int i=0; i<num1.length-1; i++){//num1.length-1 번 비교연산 수행한다.
+            boolean isSwapped = false;
             for(int j=0; j<num1.length-1-i; j++){//현재값이랑 다음에 들어올 값중에 큰값들어오면 바꿔줄거임
                 if(Math.abs(num1[j]-n)>Math.abs(num1[j+1]-n)){
                     int temp = num1[j+1];
                     num1[j+1] = num1[j];
-                    num1[j] = temp;                    
+                    num1[j] = temp;
+                    isSwapped = true;
                 }
+            }
+            if(isSwapped == false){
+                break;
             }
         }
                 
         return num1;
     }
 }
+
+
