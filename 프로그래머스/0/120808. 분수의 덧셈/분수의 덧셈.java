c325ca14f128b4bd02d55bc36033760e40fc328e
hyezuu <147456219+hyezuu@users.noreply.github.com>
2024-02-02 23:16:22 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.05 ms, Memory: 76.9 MB -BaekjoonHub

--- 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.java
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.java
@@ -1,24 +1,12 @@
 class Solution {
     public int[] solution(int numer1, int denom1, int numer2, int denom2) {
-        //기약분수로 나타내는법 -> 최대공약수로 나누기
-        //더하려면-> 통분 
-        
-        //분모 -> denom1과 denom2의 최소공배수 (최소공배수는 두 수 곱해서 최대공약수로 나누면 된다)
-        //분자 -> (최소공배수/denom1)*number1 + (최소공배수/denom2)*number2
-        
-        //위에 두줄 생략가능.. 굳이 약분안해도됨
-        
-        //최대공약수 -> 분모와 분자의 최대공약수
-        //기약분수 분모 -> 분모 / 최대공약수
-        //기약분수 분자 -> 분자 / 최대공약수       
-        int gcd1 = getGcd(Math.max(denom1, denom2),Math.min(denom1, denom2));
+
         int denom = denom1*denom2;
         int numer = denom2*numer1 + denom1*numer2 ;
         
-        int gcd2 = getGcd(Math.max(denom,numer),Math.min(denom,numer));
+        int gcd = getGcd(Math.max(denom,numer),Math.min(denom,numer));
         
-        int[] answer = {numer/gcd2 , denom/gcd2};
-        return answer ;
+        return new int[]{numer/gcd , denom/gcd};
     }
     public int getGcd (int bigger, int smaller){
         int remainder = bigger%smaller;
