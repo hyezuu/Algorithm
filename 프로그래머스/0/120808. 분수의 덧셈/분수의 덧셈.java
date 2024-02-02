81fa3da5e44a058387a87eec4000ee9e8feb71ef
hyezuu <147456219+hyezuu@users.noreply.github.com>
2024-02-02 23:09:03 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.03 ms, Memory: 73.2 MB -BaekjoonHub

--- 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.java
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.java
@@ -5,12 +5,15 @@ class Solution {
         
         //분모 -> denom1과 denom2의 최소공배수 (최소공배수는 두 수 곱해서 최대공약수로 나누면 된다)
         //분자 -> (최소공배수/denom1)*number1 + (최소공배수/denom2)*number2
+        
+        //위에 두줄 생략가능.. 굳이 약분안해도됨
+        
         //최대공약수 -> 분모와 분자의 최대공약수
         //기약분수 분모 -> 분모 / 최대공약수
         //기약분수 분자 -> 분자 / 최대공약수       
         int gcd1 = getGcd(Math.max(denom1, denom2),Math.min(denom1, denom2));
-        int denom = (denom1*denom2)/gcd1;
-        int numer = (denom/denom1)*numer1 + (denom/denom2)*numer2 ;
+        int denom = denom1*denom2;
+        int numer = denom2*numer1 + denom1*numer2 ;
         
         int gcd2 = getGcd(Math.max(denom,numer),Math.min(denom,numer));
         
