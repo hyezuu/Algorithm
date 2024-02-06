84d10a3c35bfc43e1cde01eef2c3c41c965d4181
hyezuu <147456219+hyezuu@users.noreply.github.com>
2024-02-06 09:56:14 +0900

    [Bronze II] Title: 숫자, Time: 444 ms, Memory: 31476 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/10093. 숫자/숫자.java
+++ 백준/Bronze/10093. 숫자/숫자.java
@@ -1,24 +1,24 @@
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
         Main main = new Main();
         Scanner scanner = new Scanner(System.in);
-        int a = scanner.nextInt();
-        int b = scanner.nextInt();
+        long a = scanner.nextLong();
+        long b = scanner.nextLong();
 
-        System.out.print(main.solution(a,b));
+        System.out.print(main.solution(a, b));
     }
-    public String solution(int a, int b){
-        int max = Math.max(a, b);
-        int min = Math.min(a, b);
-        int count = min==max? 0: max-min-1;
-        String answer = "";
-        for(int i=min+1; i<max ;i++){
-            answer += i+" ";
+
+    public String solution(long a, long b) {
+        long max = Math.max(a, b);
+        long min = Math.min(a, b);
+        long count = min == max ? 0 : max - min - 1;
+        StringBuilder sb = new StringBuilder();
+        for (long i = min + 1; i < max; i++) {
+            sb.append(i).append(" ");
         }
-        answer = count + "\n" + answer;
-        return answer.substring(0, answer.length()-1);
+        String answer = count + "\n" + sb.toString();
+        return answer.substring(0, answer.length() - 1);
     }
 }
