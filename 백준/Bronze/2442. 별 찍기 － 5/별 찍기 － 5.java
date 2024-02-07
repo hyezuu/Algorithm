0668d4acf1f67632cc86ba0adb573461dde6c7e9
hyezuu <147456219+hyezuu@users.noreply.github.com>
2024-02-07 18:10:30 +0900

    [Bronze III] Title: 별 찍기 - 5, Time: 124 ms, Memory: 14180 KB -BaekjoonHub

---
+++ 백준/Bronze/2442. 별 찍기 － 5/별 찍기 － 5.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
+        for (int i = 1; i <=n ; i++) {
+            bw.write(" ".repeat(n-i));
+            bw.write("*".repeat(2*i-1));
+            bw.write("\n");
+        }
+        bw.flush();
+    }
+}
