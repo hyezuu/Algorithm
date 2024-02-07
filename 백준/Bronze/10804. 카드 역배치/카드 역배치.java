b05cb5e56feed3de9b973998dd411d6d200bb9c4
hyezuu <147456219+hyezuu@users.noreply.github.com>
2024-02-07 16:28:35 +0900

    [Bronze II] Title: 카드 역배치, Time: 216 ms, Memory: 17800 KB -BaekjoonHub

---
+++ 백준/Bronze/10804. 카드 역배치/카드 역배치.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner scanner = new Scanner(System.in);
+        int[][] nums = new int[10][2];
+        for(int i=0; i<10; i++){
+            nums[i][0] = scanner.nextInt();
+            nums[i][1] = scanner.nextInt();
+            scanner.nextLine();
+        }
+        System.out.print(T.solution(nums));
+    }
+    public String solution(int[][] nums) {
+        StringBuilder sb = new StringBuilder();
+        int[] arr = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
+        int[] answer = new int[20];
+        for(int[] num : nums){
+           for(int i=0; i<20; i++){
+               if(i>=(num[0]-1)&&i<(num[0])+(num[1]-num[0])/2){
+                   int tmp=arr[num[0]+num[1]-i-2];
+                   arr[num[0]+num[1]-i-2] = arr[i];
+                   arr[i] = tmp;
+               }
+           }
+        }
+        for(int i=0; i<20; i++){
+            sb.append(arr[i]).append(" ");
+        }
+        String str = sb.toString();
+        return str.substring(0,str.length()-1);
+    }
+}
