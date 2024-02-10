98cf178249029b76105f4186a473e1cb16522344
hyezuu <147456219+hyezuu@users.noreply.github.com>
2024-02-10 13:52:31 +0900

    [Bronze II] Title: 애너그램 만들기, Time: 128 ms, Memory: 14192 KB -BaekjoonHub

---
+++ 백준/Bronze/1919. 애너그램 만들기/애너그램 만들기.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Main T = new Main();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        char[] c0 = br.readLine().toCharArray();
+        char[] c1 = br.readLine().toCharArray();
+
+        bw.write(T.solution(c0, c1));
+        bw.flush();
+    }
+
+    public String solution(char[] c0, char[] c1) {
+        int count = 0;
+        int[] arr = new int[26];
+        for (int i = 0; i <c0.length; i++) {
+            arr[c0[i] - 'a']++;
+        }
+        for (int i = 0; i <c1.length; i++) {
+            arr[c1[i] - 'a']--;
+        }
+        for (int num : arr) {
+            if (num != 0) {
+                count += Math.abs(num);
+            }
+        }
+        return count + "";
+    }
+}
