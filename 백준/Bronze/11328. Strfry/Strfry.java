d2f043d573cdca4829d3ed5b069c1cac71c49061
hyezuu <147456219+hyezuu@users.noreply.github.com>
2024-02-10 13:21:38 +0900

    [Bronze II] Title: Strfry, Time: 664 ms, Memory: 35088 KB -BaekjoonHub

--- 백준/Bronze/11328. Strfry/Strfry.java
+++ 백준/Bronze/11328. Strfry/Strfry.java
@@ -10,28 +10,18 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int count = sc.nextInt();
         sc.nextLine();
-        String[][] str = new String[count][2];
+        StringBuilder sb = new StringBuilder();
         for (int i = 0; i < count; i++) {
             String arr = sc.nextLine();
-            str[i][0] = arr.split(" ")[0];
-            str[i][1] = arr.split(" ")[1];
-        }
-        bw.write(T.solution(count, str));
-        bw.flush();
-    }
-
-    public String solution(int count, String[][] str) {
-        StringBuilder sb = new StringBuilder();
-        for (int i=0; i<count; i++) {
-            char[] c0 = str[i][0].toCharArray();
-            char[] c1 = str[i][1].toCharArray();
+            char[] c0 = arr.split(" ")[0].toCharArray();
+            char[] c1 = arr.split(" ")[1].toCharArray();
             Arrays.sort(c0);
             Arrays.sort(c1);
             if (String.valueOf(c0).equals(String.valueOf(c1))) {
                 sb.append("Possible").append("\n");
-            }
-            else sb.append("Impossible").append("\n");
+            } else sb.append("Impossible").append("\n");
         }
-        return sb.toString();
+        bw.write(sb.toString());
+        bw.flush();
     }
 }
