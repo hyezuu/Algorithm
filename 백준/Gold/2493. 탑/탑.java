a3a7b7a127e047a253fa9f92cf5928409f57cb81
hyezuu <147456219+hyezuu@users.noreply.github.com>
2024-02-21 18:03:44 +0900

    [Gold V] Title: 탑, Time: 852 ms, Memory: 93852 KB -BaekjoonHub

---
+++ 백준/Gold/2493. 탑/탑.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.*;
+
+class Tower {
+    int index;
+    int height;
+
+    public Tower(int index, int height) {
+        this.index = index;
+        this.height = height;
+    }
+}
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer nums = new StringTokenizer(br.readLine());
+        Stack<Tower> stack = new Stack<>();
+        for (int i = 0; i < n; i++) {
+            int height = Integer.parseInt(nums.nextToken());
+
+            while (!stack.isEmpty() && stack.peek().height <= height){
+                stack.pop();
+            }
+
+            if(!stack.isEmpty()) {
+                bw.write(stack.peek().index+" ");
+            }
+            else bw.write("0 ");
+
+            stack.push(new Tower(i+1, height));
+        }
+        bw.flush();
+    }
+}
