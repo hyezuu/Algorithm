894454ab99bf8ce73db6ec123e0b2c0947f59fb0
hyezuu <147456219+hyezuu@users.noreply.github.com>
2024-02-22 18:02:40 +0900

    [Bronze II] Title: 16진수, Time: 208 ms, Memory: 17688 KB -BaekjoonHub

---
+++ 백준/Bronze/1550. 16진수/16진수.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        System.out.printf("%d", Integer.parseInt(s ,16));
+    }
+}
