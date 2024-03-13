630f2a6be0122be46eabcaa30ea09496237994ef
hyezuu <147456219+hyezuu@users.noreply.github.com>
2024-03-14 03:27:08 +0900

    [level 0] Title: 공백으로 구분하기 2, Time: 0.07 ms, Memory: 75.3 MB -BaekjoonHub

--- 프로그래머스/0/181868. 공백으로 구분하기 2/공백으로 구분하기 2.java
+++ 프로그래머스/0/181868. 공백으로 구분하기 2/공백으로 구분하기 2.java
@@ -2,12 +2,6 @@ import java.util.*;
 
 class Solution {
     public String[] solution(String my_string) {
-        List<String> list = new ArrayList<>();
-        for(String x : my_string.split("\\s+")){
-            if(!x.isEmpty()){
-                list.add(x);
-            }
-        }
-        return list.toArray(new String[0]);
+        return my_string.trim().split("[ ]+");
     }
 }
