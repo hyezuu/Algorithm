4a7ad567428d2922fb5139c64846ae3979dfc4ce
hyezuu <147456219+hyezuu@users.noreply.github.com>
2024-03-14 04:02:34 +0900

    [level 0] Title: 0 떼기, Time: 0.02 ms, Memory: 73.6 MB -BaekjoonHub

--- 프로그래머스/0/181847. 0 떼기/0 떼기.java
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.java
@@ -1,5 +1,5 @@
 class Solution {
-    public String solution(String n_str) {
-        return Integer.parseInt(n_str)+"";
+    public String solution(String nStr) {
+        return String.valueOf(Integer.parseInt(nStr));
     }
 }
