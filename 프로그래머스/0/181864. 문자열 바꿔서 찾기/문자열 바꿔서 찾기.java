1cb632e8b7b8b1657bc68624a23afec058e099e6
hyezuu <147456219+hyezuu@users.noreply.github.com>
2024-03-14 04:20:09 +0900

    [level 0] Title: 문자열 바꿔서 찾기, Time: 0.02 ms, Memory: 76.4 MB -BaekjoonHub

--- 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.java
+++ 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.java
@@ -1,11 +1,5 @@
 class Solution {
     public int solution(String myString, String pat) {
-        StringBuilder sb = new StringBuilder();
-        for(char c : pat.toCharArray()){
-            if(c=='A') sb.append('B');
-            else if(c=='B') sb.append('A');
-            else sb.append(c);
-        }
-        return myString.contains(sb.toString())?1:0;
+        return myString.replace('A','a').replace('B','A').replace('a','B').contains(pat)? 1:0;
     }
 }
