957c09f7d02867d0a9235f625d07e325c0387bd6
hyezuu <147456219+hyezuu@users.noreply.github.com>
2024-03-14 18:06:09 +0900

    [level 0] Title: 수 조작하기 2, Time: 8.68 ms, Memory: 84.6 MB -BaekjoonHub

--- 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.java
+++ 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.java
@@ -2,20 +2,11 @@ class Solution {
     public String solution(int[] numLog) {
         StringBuilder sb = new StringBuilder();
         for(int i=1; i<numLog.length; i++){
-            switch(numLog[i]-numLog[i-1]){
-                case 1 : 
-                    sb.append('w');
-                    break;
-                case -1 : 
-                    sb.append('s');
-                    break;
-                case 10 : 
-                    sb.append('d');
-                    break;
-                case -10 : 
-                    sb.append('a');
-                    break;
-            }
+            int rs = numLog[i]-numLog[i-1];
+            if(rs==1) sb.append("w");
+            else if(rs==-1) sb.append("s");
+            else if(rs>1) sb.append("d");
+            else sb.append("a");
         }
         return sb.toString();
     }
