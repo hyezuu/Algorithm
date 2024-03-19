d24abeadb2df0f9fb2c52eec26a08a286970703c
hyezuu <147456219+hyezuu@users.noreply.github.com>
2024-03-19 22:34:43 +0900

    [level 0] Title: 세 개의 구분자, Time: 66.45 ms, Memory: 107 MB -BaekjoonHub

--- 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.java
+++ 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.java
@@ -1,17 +1,10 @@
-import java.util.*;
-
+import java.util.Arrays;
 class Solution {
     public String[] solution(String myStr) {
-        StringTokenizer stk = new StringTokenizer(myStr, "abc");
-        int len = stk.countTokens();
-        if(len==0) return new String[] {"EMPTY"};
-        else {
-            String[] answer = new String[len];
-            
-            for(int i=0; i<len; i++){
-                answer[i] = stk.nextToken();
-            }
-            return answer;
-        }
+        myStr = myStr.replaceAll("[a|b|c]+", ",");
+        myStr = myStr.charAt(0) == ',' ? myStr.substring(1) : myStr;
+        myStr = myStr.equals("") ? "EMPTY" : myStr;
+
+        return myStr.split("[,]");
     }
 }
