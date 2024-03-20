21dc58712c63c7555df2f1c218891d764b1ab62d
hyezuu <147456219+hyezuu@users.noreply.github.com>
2024-03-20 20:49:17 +0900

    [level 0] Title: 2의 영역, Time: 1.64 ms, Memory: 98.1 MB -BaekjoonHub

--- 프로그래머스/0/181894. 2의 영역/2의 영역.java
+++ 프로그래머스/0/181894. 2의 영역/2의 영역.java
@@ -1,22 +1,25 @@
+import java.util.*;
 class Solution {
     public int[] solution(int[] arr) {
-         boolean b = false;
-        int fi=0, li=0;
-        StringBuilder sb = new StringBuilder();
-        for (int i=0; i<arr.length; i++) {
-            if(!b && arr[i]==2){
-                fi=i;
-                li=i;
-                b= true;
-            }
-            else if(b && arr[i]==2){
-                li = i;
+        int[] answer = {};
+        int start = -1;
+        int end = -1;
+
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==2){
+                if(start==-1){
+                    start = i;
+                }
+                end = i;
             }
         }
-        int[] answer = new int[li-fi+1];
-        if(b) System.arraycopy(arr,fi,answer,0,li-fi+1);
-        else answer[0] = -1;
-        
+
+        if(start==-1){
+            answer = new int[]{-1};
+        }else{
+
+            answer = Arrays.copyOfRange(arr,start,end+1);
+        }
         return answer;
     }
 }
