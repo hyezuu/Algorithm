697aeea2e01c6d513f1ba7d30e37504df41b153d
hyezuu <147456219+hyezuu@users.noreply.github.com>
2024-03-21 18:38:58 +0900

    [level 0] Title: 리스트 자르기, Time: 0.02 ms, Memory: 74.7 MB -BaekjoonHub

--- 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.java
+++ 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.java
@@ -1,28 +1,11 @@
 class Solution {
     public int[] solution(int n, int[] slicer, int[] num_list) {
-        int a = slicer[0];
-        int b = slicer[1];
-        int c = slicer[2];
-        int[] answer;
-        if(n==1){
-            answer = new int[b+1];
-            System.arraycopy(num_list,0,answer,0,b+1);
-        }
-        else if(n==2){
-            answer = new int[num_list.length-a];
-            System.arraycopy(num_list,a,answer,0,num_list.length-a);
-        }
-        else if(n==3){
-            answer = new int[b-a+1];
-            System.arraycopy(num_list,a,answer,0,b-a+1);
-        }
-        else {
-            int len = (b-a+1)%c==0? (b-a+1)/c : (int)((b-a+1)/c)+1;
-            answer = new int[len];
-            int idx = 0;
-            for(int i=a ; i<=b; i+=c){
-                answer[idx++] = num_list[i];
-            }            
+        int start = n==1? 0:slicer[0];
+        int end = n==2? num_list.length-1:slicer[1];
+        int step = n==4? slicer[2]:1;
+        int[] answer = new int[(end-start+step)/step];
+        for(int i=start, idx=0; i<=end; i+=step){
+            answer[idx++] = num_list[i];
         }
         return answer;
     }
