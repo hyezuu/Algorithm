130c5ed9ed1fb80c7ae4d2b68fe2facf4778b72d
hyezuu <147456219+hyezuu@users.noreply.github.com>
2024-03-21 22:35:12 +0900

    [level 0] Title: 배열 만들기 6, Time: 15.95 ms, Memory: 87.2 MB -BaekjoonHub

--- 프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.java
+++ 프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.java
@@ -2,22 +2,13 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] arr) {
-        Stack<Integer> stack = new Stack<>();
-        int i = 0;
+        Stack<Integer> stack = new Stack<>(); 
         
         for(int a : arr){
             if(!stack.isEmpty()&& stack.peek()==a) stack.pop();
             else stack.push(a);
-            i++;
         }
-        if(stack.isEmpty()) return new int[]{-1};
-        else {
-            int[] answer = new int[stack.size()];
-            int idx = 0;
-            for(int k : stack){
-                answer[idx++] = k;
-            }
-            return answer;
-        }
+        
+        return stack.isEmpty()? new int[]{-1} : stack.stream().mapToInt(i->i).toArray();
     }
 }
