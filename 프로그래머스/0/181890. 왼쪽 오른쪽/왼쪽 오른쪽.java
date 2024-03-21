f64ffaf7177b280132925143b3b4d66cbfdad361
hyezuu <147456219+hyezuu@users.noreply.github.com>
2024-03-22 00:01:05 +0900

    [level 0] Title: 왼쪽 오른쪽, Time: 0.04 ms, Memory: 78.6 MB -BaekjoonHub

--- 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.java
+++ 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.java
@@ -2,18 +2,15 @@ import java.util.*;
 
 class Solution {
     public String[] solution(String[] str_list) {
-        int idx = 0;
-        String s = "";
-        
+
         for(int i=0; i<str_list.length; i++){
-            if(str_list[i].equals("l")|| str_list[i].equals("r")){
-                idx = i;
-                s = str_list[i];
-                break;
+            if(str_list[i].equals("l")){
+                return Arrays.copyOfRange(str_list,0,i);
+            }
+            else if(str_list[i].equals("r")){
+                return Arrays.copyOfRange(str_list,i+1,str_list.length);
             }
         }
-        
-        if(s.equals("r")) return Arrays.copyOfRange(str_list,idx+1,str_list.length);
-        else return Arrays.copyOfRange(str_list,0,idx);
+        return new String[0];
     }
 }
