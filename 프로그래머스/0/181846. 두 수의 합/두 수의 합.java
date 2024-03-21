3e4699f8262f24392422e7f4f683a522fe0d9ba0
hyezuu <147456219+hyezuu@users.noreply.github.com>
2024-03-22 00:10:38 +0900

    [level 0] Title: 두 수의 합, Time: 2358.51 ms, Memory: 381 MB -BaekjoonHub

--- 프로그래머스/0/181846. 두 수의 합/두 수의 합.java
+++ 프로그래머스/0/181846. 두 수의 합/두 수의 합.java
@@ -1,10 +1,59 @@
-import java.math.*;
+import java.util.Stack;
 
 class Solution {
-    public String solution(String a, String b) {
-        BigInteger A = new BigInteger(a);
-        BigInteger B = new BigInteger(b);
-        
-        return A.add(B)+"";
+
+    public static String solution(String a, String b) {
+        String answer = "";
+        Stack<Integer> stack = new Stack<>();
+        String tmp = "";
+
+        int shortLen = Math.min(a.length(), b.length());
+        int longLen = Math.max(a.length(), b.length());
+
+        if (a.length() != b.length()) {
+            if (shortLen == a.length()) {
+                for (int i = shortLen; i < longLen; i++) {
+                    tmp += "0";
+                }
+                a = tmp + a;
+            } else {
+                for (int i = shortLen; i < longLen; i++) {
+                    tmp += "0";
+                }
+                b = tmp + b;
+            }
+        }
+
+        boolean carry = false;
+        int sumNum = 0;
+
+        for (int i = a.length() - 1; i >= 0; i--) {
+            int NumA = Integer.parseInt(String.valueOf(a.charAt(i)));
+            int NumB = Integer.parseInt(String.valueOf(b.charAt(i)));
+
+            if (carry) {
+                sumNum = NumA + NumB + 1;
+                carry = false;
+            } else {
+                sumNum = NumA + NumB;
+            }
+
+            if (sumNum >= 10) {
+                carry = true;
+                stack.push(sumNum - 10);
+            } else {
+                stack.push(sumNum);
+            }
+        }
+
+        if (carry) {
+            stack.push(1);
+        }
+
+        while (!stack.isEmpty()) {
+            answer += stack.pop();
+        }
+
+        return answer;
     }
 }
