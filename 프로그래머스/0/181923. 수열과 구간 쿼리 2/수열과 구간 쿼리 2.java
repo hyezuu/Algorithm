13ea3439244f5b9511e7ff43d73e26323b422b37
hyezuu <147456219+hyezuu@users.noreply.github.com>
2024-03-22 20:45:49 +0900

    [level 0] Title: 수열과 구간 쿼리 2, Time: 2.41 ms, Memory: 80.8 MB -BaekjoonHub

--- 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.java
+++ 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.java
@@ -1,23 +1,16 @@
-import java.util.Arrays;
-
 class Solution {
     public int[] solution(int[] arr, int[][] queries) {
-
         int[] answer = new int[queries.length];
-        Arrays.fill(answer, -1);
-
-        for (int idx = 0; idx < queries.length; idx++) {
-            int[] query = queries[idx];
-            int s = query[0], e = query[1], k = query[2];
-
-            for (int i = s; i <= e; i++) {
-                if (k < arr[i]) {
-                    answer[idx] = answer[idx] == -1 ? arr[i] : Math.min(answer[idx], arr[i]);
+        int idx = 0;
+        for(int[] query :queries){
+            int min = 1000001;
+            for(int i = query[0]; i<=query[1]; i++){
+                if(arr[i]>query[2]){
+                    min = Math.min(arr[i], min);
                 }
             }
-
+            answer[idx++] = min==1000001? -1:min;
         }
-
         return answer;
     }
 }
