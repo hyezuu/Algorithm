a3036d85c13c69383d59edda19d47776ee7d784a
hyezuu <147456219+hyezuu@users.noreply.github.com>
2024-03-28 23:11:06 +0900

    [level 0] Title: 배열 조각하기, Time: 0.03 ms, Memory: 77.5 MB -BaekjoonHub

--- 프로그래머스/0/181893. 배열 조각하기/배열 조각하기.java
+++ 프로그래머스/0/181893. 배열 조각하기/배열 조각하기.java
@@ -2,19 +2,30 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] arr, int[] query) {
-        int start = 0, end = arr.length-1;
+        Deque<Integer> deque = new ArrayDeque<>();
+        
+        for(int a : arr){
+            deque.add(a);
+        }
+        int size = 0;
         for(int i=0; i<query.length; i++){
+            size = deque.size();
             if(i%2==0){
-                end = start + query[i];
+                for(int j=1; j<size - query[i] ; j++){
+                    deque.removeLast();
+                }
             }
-            else start += query[i];
+            else {
+                for(int j=0; j<query[i]; j++){
+                    deque.removeFirst();
+                }
+            }
         }
-        int[] answer = new int[end-start+1];
-        
-        for(int i=start,j=0; i<=end; i++){
-            answer[j++] = arr[i];
+        int[] answer = new int[deque.size()];
+        int idx = 0;
+        for(int a : deque){
+            answer[idx++] = a;
         }
-        
         return answer;
     }
 }
