6eb98e8fd5fbbec6fc7d5475bd5838e8a37e4c00
hyezuu <147456219+hyezuu@users.noreply.github.com>
2024-04-02 15:01:17 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.01 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -2,15 +2,11 @@ import java.util.*;
 
 public class Solution {
     public int solution(int n) {
-        String str = n+"";
         int answer = 0;
-        for(String a : str.split("")){
-            answer += Integer.parseInt(a);
+        while(n>0){
+            answer += n%10;
+            n/=10;            
         }
-
-        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-        System.out.println("Hello Java");
-
         return answer;
     }
 }
