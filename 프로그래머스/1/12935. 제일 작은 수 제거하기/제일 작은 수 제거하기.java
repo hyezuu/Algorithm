90913b6887beaf9341ab528b5924b49ef65073f3
hyezuu <147456219+hyezuu@users.noreply.github.com>
2024-04-09 21:35:22 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 0.72 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.java
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.java
@@ -1,21 +1,15 @@
 import java.util.*;
 
 class Solution {
-    public int[] solution(int[] arr) {
-        if(arr.length==1){
-            arr[0] = -1;
-            return arr;
-        }
-        int[] ca = Arrays.copyOf(arr, arr.length);
-        Arrays.sort(ca);
-        
-        int[] answer = new int[arr.length-1];
-        int idx = 0;
-        
-        for(int a : arr){
-            if(a!=ca[0]) answer[idx++] = a;
-        }
-        return answer;
-
-    }
+  public int[] solution(int[] arr) {
+    if (arr.length == 1) return new int[]{-1};
+   
+    List<Integer> list = new ArrayList<>();
+    for (int a : arr) list.add(a);
+    list.remove(Collections.min(list));
+      
+    int[] answer = new int[arr.length - 1];
+    for (int i = 0; i < list.size(); i++)answer[i] = list.get(i);
+    return answer;
+  }
 }
