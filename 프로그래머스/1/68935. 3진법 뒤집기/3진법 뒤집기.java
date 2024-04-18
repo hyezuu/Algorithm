885928c12166e5da5f13ba0e348f90487a00c27a
hyezuu <147456219+hyezuu@users.noreply.github.com>
2024-04-18 19:54:57 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.04 ms, Memory: 69.7 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -1,11 +1,10 @@
 class Solution {
     public int solution(int n) {
         StringBuilder sb = new StringBuilder();
-        while(n>=3){
+        while(n>0){
             sb.append(n%3);
             n/=3;
         }
-        sb.append(n);
         return Integer.parseInt(sb.toString(), 3);
     }
 }
