af6a6d0a92e184c7931f1d140de8fa0a30bffd37
hyezuu <147456219+hyezuu@users.noreply.github.com>
2024-04-20 19:18:05 +0900

    [level 1] Title: 예산, Time: 0.39 ms, Memory: 79.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/12982. 예산/예산.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] d, int budget) {
+        Arrays.sort(d);
+        int i;
+        for(i=0; i<d.length; i++){
+            budget-=d[i];
+            if(budget<0) return i;
+        }
+        return i;
+    }
+}
