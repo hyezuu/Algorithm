c921123d435b6936e52006677f95608a11c19339
hyezuu <147456219+hyezuu@users.noreply.github.com>
2024-04-24 16:37:00 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.05 ms, Memory: 75.5 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -3,15 +3,12 @@ class Solution {
         String[] answer = new String[n];
         for(int i=0; i<n; i++){
             StringBuilder sb = new StringBuilder();
-            String idx1 = Integer.toBinaryString(arr1[i]);
-            String idx2 = Integer.toBinaryString(arr2[i]);
+            int tmp = arr1[i]|arr2[i];
+            int w = n;
             
-            idx1 = "0".repeat(n-idx1.length())+idx1;
-            idx2 = "0".repeat(n-idx2.length())+idx2;
-            
-            for(int j=0; j<n; j++){
-                boolean chk = idx1.charAt(j)=='1'||idx2.charAt(j)=='1';
-                sb.append(chk ? "#" : " ");
+            while(w--!=0){
+                sb.insert(0, tmp%2 == 0? " " : "#");
+                tmp>>=1; 
             }
             answer[i] = sb.toString();
         }
