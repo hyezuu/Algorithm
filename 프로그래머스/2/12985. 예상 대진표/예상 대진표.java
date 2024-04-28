2f5800579c68bd451d7839cff4e3e7343e23fa81
hyezuu <147456219+hyezuu@users.noreply.github.com>
2024-04-28 21:56:00 +0900

    [level 2] Title: 예상 대진표, Time: 0.02 ms, Memory: 89.2 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
@@ -3,8 +3,8 @@ class Solution
     public int solution(int n, int a, int b){
         int answer = 0;
         while(a!=b){
-            a = (int)a/2 + (a%2==0 ? 0 : 1);
-            b = (int)b/2 + (b%2==0 ? 0 : 1);
+            a = a/2 + a%2;
+            b = b/2 + b%2;
             answer++;
         }
         return answer;
