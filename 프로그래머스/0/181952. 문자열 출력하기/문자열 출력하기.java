3e734dcdd17638b974fd7f2d0f650d3db3c4d75c
zheyuu <147456219+hyezuu@users.noreply.github.com>
2024-05-22 19:29:45 +0900

    [level 0] Title: 문자열 출력하기, Time: 150.32 ms, Memory: 59.5 MB -BaekjoonHub

--- 프로그래머스/0/181952. 문자열 출력하기/문자열 출력하기.java
+++ 프로그래머스/0/181952. 문자열 출력하기/문자열 출력하기.java
@@ -1,9 +1,11 @@
-import java.util.Scanner;
+import java.io.*;
 
 public class Solution {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String a = sc.next();
-        System.out.println(a);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        bw.write(br.readLine());
+        bw.flush();
     }
 }
