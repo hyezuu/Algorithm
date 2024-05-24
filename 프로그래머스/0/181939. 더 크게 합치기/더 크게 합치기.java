44f48a5d512a0811652010707ea643dc60a5b537
zheyuu <147456219+hyezuu@users.noreply.github.com>
2024-05-24 17:35:55 +0900

    [level 0] Title: 더 크게 합치기, Time: 9.67 ms, Memory: 73.6 MB -BaekjoonHub

--- 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.java
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.java
@@ -2,6 +2,6 @@ class Solution {
     public int solution(int a, int b) {
         int c = Integer.parseInt(a+""+b+"");
         int d = Integer.parseInt(b+""+a+"");
-        return c>d ? c:d;
+        return Math.max(c,d);
     }
 }
