2ff69ebc31cd8f97dad3fa4ac03e2821cba3b4ed
zheyuu <147456219+hyezuu@users.noreply.github.com>
2024-05-28 13:26:06 +0900

    [level 0] Title: 수열과 구간 쿼리 2, Time: 10.64 ms, Memory: 86.6 MB -BaekjoonHub

--- 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.java
+++ 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.java
@@ -1,16 +1,27 @@
+import java.util.*;
+
 class Solution {
     public int[] solution(int[] arr, int[][] queries) {
         int[] answer = new int[queries.length];
-        int idx = 0;
-        for(int[] query :queries){
-            int min = 1000001;
-            for(int i = query[0]; i<=query[1]; i++){
-                if(arr[i]>query[2]){
-                    min = Math.min(arr[i], min);
-                }
+        int i=0 ; 
+        
+        for(int[] query : queries) {
+            int s = query [0], e = query [1], k= query [2];
+            int[] tmp = new int[e-s+1];
+            
+            System.arraycopy(arr, s, tmp, 0, tmp.length);
+            Arrays.sort(tmp);
+            
+            int value = -1;
+            for(int n : tmp){
+                if(n>k){
+                    value=n;
+                    break;
+                } 
             }
-            answer[idx++] = min==1000001? -1:min;
+            answer[i++] = value;
         }
         return answer;
+        
     }
 }
