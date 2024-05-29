9224e2b52eaed162d5bd954f5e8bfc73a390c162
zheyuu <147456219+hyezuu@users.noreply.github.com>
2024-05-29 21:01:49 +0900

    [level 0] Title: 배열 만들기 4, Time: 7.04 ms, Memory: 81.3 MB -BaekjoonHub

--- 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.java
+++ 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.java
@@ -2,18 +2,32 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] arr) {
-        Stack<Integer> stack = new Stack<>();
-        int i = 0;
+        List<Integer> list = new ArrayList<>();
+        int i=0;
         while(i<arr.length){
-            if(stack.isEmpty()||stack.peek()<arr[i]) stack.push(arr[i++]);
-            else stack.pop();
+            int last = list.size()-1;
+            if(list.isEmpty()||list.get(last)<arr[i]) list.add(arr[i++]);
+            else list.remove(last);
         }
-        
-        int[] stk = new int[stack.size()];
-        int idx = 0;
-        for(int k : stack){
-            stk[idx++] = k;
-        }
-        return stk;
+        return list.stream().mapToInt(n->n).toArray();
     }
 }
+// import java.util.*;
+
+// class Solution {
+//     public int[] solution(int[] arr) {
+//         Stack<Integer> stack = new Stack<>();
+//         int i = 0;
+//         while(i<arr.length){
+//             if(stack.isEmpty()||stack.peek()<arr[i]) stack.push(arr[i++]);
+//             else stack.pop();
+//         }
+        
+//         int[] stk = new int[stack.size()];
+//         int idx = 0;
+//         for(int k : stack){
+//             stk[idx++] = k;
+//         }
+//         return stk;
+//     }
+// }
