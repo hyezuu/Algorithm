5347911788b7517342a99547f0e0e39c3aaab0cb
zheyuu <147456219+hyezuu@users.noreply.github.com>
2024-05-29 18:51:13 +0900

    [level 0] Title: 수열과 구간 쿼리 4, Time: 0.04 ms, Memory: 77 MB -BaekjoonHub

--- 프로그래머스/0/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.java
+++ 프로그래머스/0/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.java
@@ -1,10 +1,11 @@
 class Solution {
     public int[] solution(int[] arr, int[][] queries) {
         for(int[] query : queries){  
-            for(int i=query[0]; i<=query[1]; i++){
-                if(i%query[2]==0) arr[i]++;
+            int s =query[0], e = query[1], k = query[2];           
+            for(int i=s*k; i<=e; i+=k){
+                arr[i]++;
             }
         }
-        return arr;
+        return arr;  
     }
 }
