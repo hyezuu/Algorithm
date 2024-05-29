64eb0cf757995e24aeefb256c4add01eb7d992f2
zheyuu <147456219+hyezuu@users.noreply.github.com>
2024-05-29 19:24:18 +0900

    [level 0] Title: 배열 만들기 2, Time: 0.29 ms, Memory: 76.5 MB -BaekjoonHub

--- 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.java
+++ 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.java
@@ -3,6 +3,7 @@ import java.util.*;
 class Solution {
     public int[] solution(int l, int r) {
         ArrayList<Integer> list = new ArrayList<>();
+       
         for(int i=1; i<64; i++){
             int num = Integer.parseInt(Integer.toBinaryString(i))*5;
             if(num >=l && num <=r) list.add(num);
