f4cb630d58dd1e58c7470eae10c3650cdb07e1d4
hyezuu <147456219+hyezuu@users.noreply.github.com>
2024-05-30 18:04:12 +0900

    [level 0] Title: 9로 나눈 나머지, Time: 0.03 ms, Memory: 77.4 MB -BaekjoonHub

--- 프로그래머스/0/181914. 9로 나눈 나머지/9로 나눈 나머지.java
+++ 프로그래머스/0/181914. 9로 나눈 나머지/9로 나눈 나머지.java
@@ -1,9 +1,15 @@
 class Solution {
     public int solution(String number) {
+        // int sum = 0;
+        // for(char c : number.toCharArray()){
+        //     sum+=c-'0';
+        // }
+        // return sum%9;
+        //왜 아래가 더 느리지 
         int sum = 0;
         for(char c : number.toCharArray()){
-            sum+=c-'0';
+            sum+=c;
         }
-        return sum%9;
+        return (sum-48*number.length())%9;
     }
 }
