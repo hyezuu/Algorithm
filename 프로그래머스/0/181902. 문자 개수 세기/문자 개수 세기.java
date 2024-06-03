17dc8dccb862a78118966b31d55e4142b512243d
hyezuu <147456219+hyezuu@users.noreply.github.com>
2024-06-03 16:52:19 +0900

    [level 0] Title: 문자 개수 세기, Time: 0.03 ms, Memory: 80 MB -BaekjoonHub

--- 프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.java
+++ 프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.java
@@ -2,10 +2,8 @@ class Solution {
     public int[] solution(String my_string) {
         int[] answer = new int[52];
         for(char c : my_string.toCharArray()){
-            if(c<'a'){
-                answer[c-'A']++;
-            }
-            else answer[c-'a'+26]++;
+            int idx = Character.isUpperCase(c) ? c-'A' : c-'a'+26; 
+            answer[idx]++;
         }
         return answer;
     }
