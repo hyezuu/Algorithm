a6bf0eb8d7507f77b31c0f5f223e0f96803360b9
hyezuu <147456219+hyezuu@users.noreply.github.com>
2024-06-04 21:54:09 +0900

    [level 0] Title: 배열 조각하기, Time: 0.02 ms, Memory: 73.6 MB -BaekjoonHub

--- 프로그래머스/0/181893. 배열 조각하기/배열 조각하기.java
+++ 프로그래머스/0/181893. 배열 조각하기/배열 조각하기.java
@@ -2,18 +2,16 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] arr, int[] query) {
-        int start = 0; 
-        int end = arr.length-1;
-        for(int i=0; i<query.length; i++){
-            if(i%2==0){
-                end = start + query[i];
-            }
-            else start += query[i];
-        }
-        int len = end-start+1;
-        int[] answer = new int[len];
+        int s = 0;
+        int e = arr.length-1;
         
-        System.arraycopy(arr, start, answer, 0, len);
+        for(int i=0; i<query.length; i++){
+            if(i%2==0) e = s + query[i];
+            else s += query[i];
+        }
+        int[] answer = new int[e-s+1];
+        System.arraycopy(arr, s, answer, 0, e-s+1);
+        
         return answer;
     }
 }
