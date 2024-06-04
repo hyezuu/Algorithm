13ced59cde0ed720abc94b4b307d68d9197497aa
hyezuu <147456219+hyezuu@users.noreply.github.com>
2024-06-04 18:21:18 +0900

    [level 0] Title: 2의 영역, Time: 0.88 ms, Memory: 106 MB -BaekjoonHub

--- 프로그래머스/0/181894. 2의 영역/2의 영역.java
+++ 프로그래머스/0/181894. 2의 영역/2의 영역.java
@@ -1,25 +1,45 @@
 import java.util.*;
 class Solution {
     public int[] solution(int[] arr) {
-        int[] answer = {};
-        int start = -1;
-        int end = -1;
-
-        for(int i=0;i<arr.length;i++){
-            if(arr[i]==2){
-                if(start==-1){
-                    start = i;
-                }
-                end = i;
+        int lt= 0;
+        int rt= arr.length-1;
+        
+        int s = -1;
+        int e = -1;
+        while(lt<=rt){
+            if(arr[lt]==2 && s == -1){
+                s = lt;
             }
+            if(arr[rt]==2 && e == -1){
+                e = rt;
+            }
+            lt++;
+            rt--;
         }
-
-        if(start==-1){
-            answer = new int[]{-1};
-        }else{
-
-            answer = Arrays.copyOfRange(arr,start,end+1);
-        }
+        if(s<0 && e<0) return new int[]{-1};
+        s = s<0 ? e: s;
+        e = e<0 ? s: e;
+        
+        int[] answer = new int[e-s+1];
+        System.arraycopy(arr , s , answer, 0 , e-s+1);      
+        
         return answer;
+//       boolean b = false;
+//         int fi=0, li=0;
+//         for (int i=0; i<arr.length; i++) {
+//             if(arr[i]==2){
+//                 if(!b){
+//                     fi=i;
+//                     li=i;
+//                     b= true;
+//                 }
+//                 else li = i;
+//             }
+//         }
+//         int[] answer = new int[li-fi+1];
+//         if(b) System.arraycopy(arr,fi,answer,0,li-fi+1);
+//         else answer[0] = -1;
+
+//         return answer;
     }
 }
