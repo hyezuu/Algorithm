a2d96bd421f500d6b7cbc93269fad73886d03f12
hyezuu <147456219+hyezuu@users.noreply.github.com>
2024-06-05 10:38:09 +0900

    [level 0] Title: n 번째 원소부터, Time: 0.02 ms, Memory: 73.8 MB -BaekjoonHub

--- 프로그래머스/0/181892. n 번째 원소부터/n 번째 원소부터.java
+++ 프로그래머스/0/181892. n 번째 원소부터/n 번째 원소부터.java
@@ -1,10 +1,27 @@
 class Solution {
     public int[] solution(int[] num_list, int n) {
         int len = num_list.length-n+1;
+        
         int[] answer = new int[len];
-        for(int i=0; i<len; i++){
-            answer[i]= num_list[n-1+i];
-        }
+        System.arraycopy(num_list, n-1, answer, 0, len);
         return answer;
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        // int len = num_list.length-n+1;
+        // int[] answer = new int[len];
+        // for(int i=0; i<len; i++){
+        //     answer[i]= num_list[n-1+i];
+        // }
+        // return answer;
     }
 }
