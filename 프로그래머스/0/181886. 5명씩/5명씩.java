7c91ab3eaec825ce2ed7e6f810d088ed95fa0f82
hyezuu <147456219+hyezuu@users.noreply.github.com>
2024-06-07 14:02:15 +0900

    [level 0] Title: 5명씩, Time: 0.02 ms, Memory: 93.9 MB -BaekjoonHub

--- 프로그래머스/0/181886. 5명씩/5명씩.java
+++ 프로그래머스/0/181886. 5명씩/5명씩.java
@@ -1,10 +1,9 @@
 class Solution {
-    public String[] solution(String[] names) 
-{
-        int len = names.length%5==0 ? names.length/5 : (names.length/5)+1;
-        String[] answer = new String[len];
-        int idx =0;
-        for(int i=0; i<names.length; i+=5){
+    public String[] solution(String[] names) {
+        int len = names.length;
+        String[] answer = new String[len/5 + (len%5==0 ? 0 : 1)];
+        int idx = 0;
+        for(int i=0; i<len; i+=5){
             answer[idx++] = names[i];
         }
         return answer;
