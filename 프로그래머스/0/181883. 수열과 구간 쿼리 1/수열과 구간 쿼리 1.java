a6e74986e369a1c5b2ab156e3bacda4fb7068090
hyezuu <147456219+hyezuu@users.noreply.github.com>
2024-06-07 14:33:40 +0900

    [level 0] Title: 수열과 구간 쿼리 1, Time: 1.48 ms, Memory: 77.1 MB -BaekjoonHub

--- 프로그래머스/0/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.java
+++ 프로그래머스/0/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.java
@@ -1,8 +1,24 @@
 class Solution {
     public int[] solution(int[] arr, int[][] queries) {
         for(int[] query : queries){
-            for(int i=query[0]; i<=query[1]; i++)arr[i]++;
+            for(int i=query[0]; i<=query[1]; i++){
+                arr[i]++;
+            }
         }
         return arr;
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        // for(int[] query : queries){
+        //     for(int i=query[0]; i<=query[1]; i++)arr[i]++;
+        // }
+        // return arr;
     }
 }
