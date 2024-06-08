575a0f338730cad357fe0d8da0f8cd98628ff14a
hyezuu <147456219+hyezuu@users.noreply.github.com>
2024-06-08 09:38:31 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 2, Time: 0.03 ms, Memory: 75.3 MB -BaekjoonHub

--- 프로그래머스/0/181881. 조건에 맞게 수열 변환하기 2/조건에 맞게 수열 변환하기 2.java
+++ 프로그래머스/0/181881. 조건에 맞게 수열 변환하기 2/조건에 맞게 수열 변환하기 2.java
@@ -1,15 +1,17 @@
 class Solution {
     public int solution(int[] arr) {
-        int max = 0;
-        for(int i=0; i<arr.length; i++){
+        
+        int max = 0;        
+        for(int num : arr){
             int cnt = 0;
-            while((arr[i]>=50&&arr[i]%2==0)||(arr[i]<50&&arr[i]%2!=0)){
-                if(arr[i]>=50&&arr[i]%2==0) arr[i]/=2;
-                else arr[i]=arr[i]*2+1;
+            
+            while(num>=50 && num%2==0||num<50 && num%2==1){
+                num = num>=50 && num%2==0 ? num/2 : (num*2)+1;
                 cnt++;
             }
-            max = Math.max(max,cnt);
+            max = Math.max(max, cnt);
         }
+        
         return max;
     }
 }
