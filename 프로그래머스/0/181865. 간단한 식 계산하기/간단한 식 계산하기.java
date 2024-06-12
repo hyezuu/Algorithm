9ae81bbfd1ebc83e4436ff62f2ebdc2ad7e3645f
hyezuu <147456219+hyezuu@users.noreply.github.com>
2024-06-12 15:49:16 +0900

    [level 0] Title: 간단한 식 계산하기, Time: 0.09 ms, Memory: 76.2 MB -BaekjoonHub

--- 프로그래머스/0/181865. 간단한 식 계산하기/간단한 식 계산하기.java
+++ 프로그래머스/0/181865. 간단한 식 계산하기/간단한 식 계산하기.java
@@ -1,16 +1,27 @@
 class Solution {
     public int solution(String binomial) {
         String[] arr = binomial.split(" ");
+        
         int a = Integer.parseInt(arr[0]);
         int b = Integer.parseInt(arr[2]);
+        
         String op = arr[1];
         
-        if(op.equals("+")){
-            return a+b;
-        }
-        else if(op.equals("-")){
-            return a-b;
-        }
-        else return a*b;
+        if(op.equals("+")) return a+b;
+        if(op.equals("-")) return a-b;
+        
+        return a*b;        
+//         String[] arr = binomial.split(" ");
+//         int a = Integer.parseInt(arr[0]);
+//         int b = Integer.parseInt(arr[2]);
+//         String op = arr[1];
+        
+//         if(op.equals("+")){
+//             return a+b;
+//         }
+//         else if(op.equals("-")){
+//             return a-b;
+//         }
+//         else return a*b;
     }
 }
