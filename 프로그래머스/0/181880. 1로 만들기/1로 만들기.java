07fe249b4bd1e5c452ea6538acd7281d7a66c899
hyezuu <147456219+hyezuu@users.noreply.github.com>
2024-06-12 17:47:19 +0900

    [level 0] Title: 1로 만들기, Time: 0.01 ms, Memory: 76.8 MB -BaekjoonHub

--- 프로그래머스/0/181880. 1로 만들기/1로 만들기.java
+++ 프로그래머스/0/181880. 1로 만들기/1로 만들기.java
@@ -2,7 +2,11 @@ class Solution {
     public int solution(int[] num_list) {
         int answer = 0;
         for(int a : num_list){
-            answer += Integer.toBinaryString(a).length()-1;
+            while(a>1){
+                a=  a>>1;
+                answer++;
+            }
+           // answer += Integer.toBinaryString(a).length()-1;
         }
         return answer;
     }
