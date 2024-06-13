e247aa7e27dd4bea8d4ad37095faeae867c563bf
hyezuu <147456219+hyezuu@users.noreply.github.com>
2024-06-13 18:28:01 +0900

    [level 0] Title: 무작위로 K개의 수 뽑기, Time: 0.43 ms, Memory: 80.4 MB -BaekjoonHub

--- 프로그래머스/0/181858. 무작위로 K개의 수 뽑기/무작위로 K개의 수 뽑기.java
+++ 프로그래머스/0/181858. 무작위로 K개의 수 뽑기/무작위로 K개의 수 뽑기.java
@@ -2,13 +2,35 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] arr, int k) {
-        Set<Integer> set = new LinkedHashSet<>();
+        Set<Integer> set = new HashSet<>();
         int[] answer = new int[k];
         Arrays.fill(answer,-1);
-        for(int i=0, j=0; i<arr.length; i++){
-            if(j==k) break;
-            if(set.add(arr[i])) answer[j++] = arr[i];
+        
+        int idx = 0;
+        for(int a : arr){
+            if(idx==k) break;
+            if(set.add(a)) answer[idx++] = a;
         }
+        
         return answer;
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        // Set<Integer> set = new LinkedHashSet<>();
+        // int[] answer = new int[k];
+        // Arrays.fill(answer,-1);
+        // for(int i=0, j=0; i<arr.length; i++){
+        //     if(j==k) break;
+        //     if(set.add(arr[i])) answer[j++] = arr[i];
+        // }
+        // return answer;
     }
 }
