6bd96e5518aa26418d6d6eb64f005b5cebd738d8
hyezuu <147456219+hyezuu@users.noreply.github.com>
2024-06-13 18:02:44 +0900

    [level 0] Title: 빈 배열에 추가, 삭제하기, Time: 3.13 ms, Memory: 76.7 MB -BaekjoonHub

--- 프로그래머스/0/181860. 빈 배열에 추가， 삭제하기/빈 배열에 추가， 삭제하기.java
+++ 프로그래머스/0/181860. 빈 배열에 추가， 삭제하기/빈 배열에 추가， 삭제하기.java
@@ -2,19 +2,40 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] arr, boolean[] flag) {
-        Stack<Integer> stack = new Stack<>();
+        List<Integer> list = new ArrayList<>();
+        
         for(int i=0; i<arr.length; i++){
-            if(flag[i]){
-                for(int j=0; j<arr[i]*2; j++)stack.push(arr[i]);
+            for(int j=0; j<arr[i]; j++){
+                if(flag[i]){
+                    list.add(arr[i]);
+                    list.add(arr[i]);
+                }
+                else list.remove(list.size()-1);
             }
-            else 
-                for(int j=0; j<arr[i]; j++)stack.pop();       
         }
-        int[] answer = new int[stack.size()];
-        int idx = 0;
-        for(int a : stack){
-            answer[idx++] = a;
-        }
-        return answer;
+        return list.stream().mapToInt(Integer::intValue).toArray();
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        // Stack<Integer> stack = new Stack<>();
+        // for(int i=0; i<arr.length; i++){
+        //     if(flag[i]){
+        //         for(int j=0; j<arr[i]*2; j++)stack.push(arr[i]);
+        //     }
+        //     else 
+        //         for(int j=0; j<arr[i]; j++)stack.pop();       
+        // }
+        // int[] answer = new int[stack.size()];
+        // int idx = 0;
+        // for(int a : stack){
+        //     answer[idx++] = a;
+        // }
+        // return answer;
     }
 }
