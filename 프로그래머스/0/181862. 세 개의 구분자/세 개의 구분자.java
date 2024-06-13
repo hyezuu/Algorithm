97d64600833bf7ab64e7d7e151398891a87864c1
hyezuu <147456219+hyezuu@users.noreply.github.com>
2024-06-13 17:23:56 +0900

    [level 0] Title: 세 개의 구분자, Time: 54.21 ms, Memory: 108 MB -BaekjoonHub

--- 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.java
+++ 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.java
@@ -1,10 +1,16 @@
 import java.util.Arrays;
 class Solution {
     public String[] solution(String myStr) {
-        myStr = myStr.replaceAll("[a|b|c]+", ",");
-        myStr = myStr.charAt(0) == ',' ? myStr.substring(1) : myStr;
-        myStr = myStr.equals("") ? "EMPTY" : myStr;
+        String[] arr = Arrays.stream(myStr.split("[a|b|c]+"))
+            .filter(x -> !x.isEmpty())
+            .toArray(String[]::new);
+        
+        return arr.length<1 ? new String[]{"EMPTY"} : arr;
+        
+//         myStr = myStr.replaceAll("[a|b|c]+", ",");
+//         myStr = myStr.charAt(0) == ',' ? myStr.substring(1) : myStr;
+//         myStr = myStr.equals("") ? "EMPTY" : myStr;
 
-        return myStr.split("[,]");
+//         return myStr.split("[,]");
     }
 }
