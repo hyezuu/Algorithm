e7d592dc88b9c3141445376b0332897f6a00dfbd
hyezuu <147456219+hyezuu@users.noreply.github.com>
2024-06-15 19:04:00 +0900

    [level 0] Title: 전국 대회 선발 고사, Time: 0.03 ms, Memory: 73.2 MB -BaekjoonHub

--- 프로그래머스/0/181851. 전국 대회 선발 고사/전국 대회 선발 고사.java
+++ 프로그래머스/0/181851. 전국 대회 선발 고사/전국 대회 선발 고사.java
@@ -1,16 +1,37 @@
 import java.util.*;
 class Solution {
     public int solution(int[] rank, boolean[] attendance) {
-        PriorityQueue<Integer> que = new PriorityQueue<>((a,b)->rank[a]-rank[b]);
+        int[] arr = new int[101];
+        Arrays.fill(arr, -1);
+        for(int i=0; i<rank.length; i++){
+            if(attendance[i]){
+                arr[rank[i]] = i; 
+            }
+        }
+        int k = 10000;
+        int answer = 0;
+        for(int idx : arr){
+            if(k<0) break;
+            if(idx!=-1){
+                answer+= k*idx;
+                k/=100;
+            } 
+        }
         
-        for(int i=0; i<attendance.length; i++){
-            if(attendance[i]) que.offer(i);
-        }
-        int cal = 10000, answer = 0;
-        for(int i=0; i<3; i++){
-            answer += que.poll()*cal;
-            cal/=100;
-        }
         return answer;
+        
+        
+           
+//         PriorityQueue<Integer> que = new PriorityQueue<>((a,b)->rank[a]-rank[b]);
+        
+//         for(int i=0; i<attendance.length; i++){
+//             if(attendance[i]) que.offer(i);
+//         }
+//         int cal = 10000, answer = 0;
+//         for(int i=0; i<3; i++){
+//             answer += que.poll()*cal;
+//             cal/=100;
+//         }
+//         return answer;
     }
 }
