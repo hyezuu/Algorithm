b7533f873c2fb0372a3a51c92bd2ac148562e428
hyezuu <147456219+hyezuu@users.noreply.github.com>
2024-06-15 18:22:40 +0900

    [level 0] Title: 뒤에서 5등 위로, Time: 0.36 ms, Memory: 75.7 MB -BaekjoonHub

--- 프로그래머스/0/181852. 뒤에서 5등 위로/뒤에서 5등 위로.java
+++ 프로그래머스/0/181852. 뒤에서 5등 위로/뒤에서 5등 위로.java
@@ -2,9 +2,21 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] num_list) {
-        int[] answer = new int[num_list.length-5];
         Arrays.sort(num_list);
-        System.arraycopy(num_list,5,answer,0,num_list.length-5);
-        return answer;
+        return Arrays.copyOfRange(num_list, 5, num_list.length);
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        // int[] answer = new int[num_list.length-5];
+        // Arrays.sort(num_list);
+        // System.arraycopy(num_list,5,answer,0,num_list.length-5);
+        // return answer;
     }
 }
