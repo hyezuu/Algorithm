402a960d8e70255aa997946fdc0a1855a21f96d8
hyezuu <147456219+hyezuu@users.noreply.github.com>
2024-06-17 13:10:57 +0900

    [level 0] Title: 두 수의 합, Time: 112.74 ms, Memory: 83.4 MB -BaekjoonHub

--- 프로그래머스/0/181846. 두 수의 합/두 수의 합.java
+++ 프로그래머스/0/181846. 두 수의 합/두 수의 합.java
@@ -1,7 +1,31 @@
-import java.math.*;
-
 class Solution {
     public String solution(String a, String b) {
-        return new BigInteger(a).add(new BigInteger(b)) + "";
+        StringBuilder sb = new StringBuilder();
+        
+        char[] arrA = a.toCharArray();
+        char[] arrB = b.toCharArray();
+        
+        int lenA = arrA.length;
+        int lenB = arrB.length;
+        
+        int carry = 0;
+        
+        for(int i = 1; i <= Math.max(lenA, lenB); i++) {
+            int sum = carry;
+            if(lenA - i >= 0) {
+                sum += arrA[lenA - i] - '0';
+            }
+            if(lenB - i >= 0) {
+                sum += arrB[lenB - i] - '0';
+            }
+            carry = sum / 10;
+            sb.insert(0, sum % 10);
+        }
+        // 마지막 자리 올림이 남아있는 경우
+        if(carry > 0) {
+            sb.insert(0, carry);
+        }
+        
+        return sb.toString();
     }
 }
