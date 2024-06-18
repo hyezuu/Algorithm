83ca3a6d7ef54f75447fe7aabc31ddfc322c289b
hyezuu <147456219+hyezuu@users.noreply.github.com>
2024-06-18 09:40:25 +0900

    [level 0] Title: 정수 찾기, Time: 0.39 ms, Memory: 76.7 MB -BaekjoonHub

--- 프로그래머스/0/181840. 정수 찾기/정수 찾기.java
+++ 프로그래머스/0/181840. 정수 찾기/정수 찾기.java
@@ -3,14 +3,14 @@ import java.util.*;
 class Solution {
     public int solution(int[] num_list, int n) {
         Arrays.sort(num_list);
-        int lt=0, rt=num_list.length-1;
+        int lt= 0;
+        int rt = num_list.length-1;
         while(lt<=rt){
-            int mid=(lt+rt)/2;
-            if(num_list[mid]==n)return 1;
-            else if(num_list[mid]>n){
-                rt = mid-1;
+            if(num_list[lt]==n || num_list[rt]==n){
+                return 1;
             }
-            else lt = mid+1;
+            lt++;
+            rt--;
         }
         return 0;
     }
