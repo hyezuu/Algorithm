5ffd11e88db7d4a841c531fd2c223836adc6775f
hyezuu <147456219+hyezuu@users.noreply.github.com>
2024-06-18 09:47:16 +0900

    [level 0] Title: 주사위 게임 1, Time: 0.03 ms, Memory: 73.8 MB -BaekjoonHub

--- 프로그래머스/0/181839. 주사위 게임 1/주사위 게임 1.java
+++ 프로그래머스/0/181839. 주사위 게임 1/주사위 게임 1.java
@@ -1,10 +1,12 @@
 class Solution {
     public int solution(int a, int b) {
-        boolean oddA = a%2!=0;
-        boolean oddB = b%2!=0;
+        boolean isAodd = a%2==1;
+        boolean isBodd = b%2==1;
         
-        if(oddA&&oddB) return (a*a)+(b*b);
-        else if(oddA||oddB) return 2*(a+b);
-        else return Math.abs(a-b);
+        if(isAodd && isBodd) return (a*a) + (b*b);
+        else if(isAodd || isBodd) return (a+b)*2;
+        
+        
+        return Math.abs(a-b);
     }
 }
