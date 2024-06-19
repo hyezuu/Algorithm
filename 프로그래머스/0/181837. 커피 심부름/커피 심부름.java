47f8fac4ab4b52bc9492a50c138199134863620b
hyezuu <147456219+hyezuu@users.noreply.github.com>
2024-06-19 10:43:18 +0900

    [level 0] Title: 커피 심부름, Time: 0.06 ms, Memory: 77.1 MB -BaekjoonHub

--- 프로그래머스/0/181837. 커피 심부름/커피 심부름.java
+++ 프로그래머스/0/181837. 커피 심부름/커피 심부름.java
@@ -1,12 +1,10 @@
 class Solution {
     public int solution(String[] order) {
         int sum = 0;
-        for(String str : order){
-            if(str.contains("latte")){
-                sum+=5000;
-            }
-            else sum+=4500;
+        for(String coffee : order){
+            sum += coffee.contains("cafelatte") ? 5000 : 4500 ;
         }
+        
         return sum;
     }
 }
