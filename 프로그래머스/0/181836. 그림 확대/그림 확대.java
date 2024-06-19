6d8f1e23413e089fda5c189b801a1d2219d35149
hyezuu <147456219+hyezuu@users.noreply.github.com>
2024-06-19 11:05:03 +0900

    [level 0] Title: 그림 확대, Time: 0.05 ms, Memory: 79.9 MB -BaekjoonHub

--- 프로그래머스/0/181836. 그림 확대/그림 확대.java
+++ 프로그래머스/0/181836. 그림 확대/그림 확대.java
@@ -1,18 +1,37 @@
 class Solution {
     public String[] solution(String[] picture, int k) {
-        int len = picture.length, idx = 0;
+        int len = picture.length;
         String[] answer = new String[len*k];
         for(int i=0; i<len; i++){
             StringBuilder sb = new StringBuilder();
-            String[] spl = picture[i].split("");
-            for(int j=0; j<picture[i].length(); j++){
-                sb.append(spl[j].repeat(k));
+            for(char p : picture[i].toCharArray()){
+                sb.append(String.valueOf(p).repeat(k));
             }
-            for(int l=0; l<k; l++){
-                answer[idx++] = sb.toString();
+            
+            for(int j=0; j<k; j++){
+                answer[i*k+j] = sb.toString();
             }
         }
         
         return answer;
+        
+        
+        
+        
+        
+//         int len = picture.length, idx = 0;
+//         String[] answer = new String[len*k];
+//         for(int i=0; i<len; i++){
+//             StringBuilder sb = new StringBuilder();
+//             String[] spl = picture[i].split("");
+//             for(int j=0; j<picture[i].length(); j++){
+//                 sb.append(spl[j].repeat(k));
+//             }
+//             for(int l=0; l<k; l++){
+//                 answer[idx++] = sb.toString();
+//             }
+//         }
+        
+//         return answer;
     }
 }
