ba370a7a2ba413f7532e6c292d6e5a243f62e3b9
hyezuu <147456219+hyezuu@users.noreply.github.com>
2024-06-20 15:49:09 +0900

    [level 0] Title: 정수를 나선형으로 배치하기, Time: 0.10 ms, Memory: 77.2 MB -BaekjoonHub

--- 프로그래머스/0/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.java
+++ 프로그래머스/0/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.java
@@ -1,22 +1,54 @@
 class Solution {
     public int[][] solution(int n) {
-        int[] posy = {1, 0, -1, 0};
-        int[] posx = {0, 1, 0, -1};
+        int[] dx = {0, 1, 0, -1};
+        int[] dy = {1, 0, -1, 0};
         
         int[][] answer = new int[n][n];
-        
-        int nx = 0, ny = 0;
-         
-        for(int i=1, j=0; i<=n*n; i++){
+        int nx = 0;
+        int ny = 0;
+        int idx = 0;
+        int posIdx = 0;
+        for(int i=1; i<=n*n; i++){
             answer[nx][ny] = i;
+            int tmpX = nx + dx[idx%4];
+            int tmpY = ny + dy[idx%4];
             
-            if(nx+posx[j%4] < 0 || nx+posx[j%4] >= n || ny+posy[j%4] < 0 || ny+posy[j%4] >= n || answer[nx+posx[j%4]][ny+posy[j%4]]!=0){
-                j++;
+            if(tmpX < 0 || tmpX >= n || tmpY < 0 || tmpY >= n || answer[tmpX][tmpY] != 0){
+                idx++;
+                nx+= dx[idx%4];
+                ny+= dy[idx%4];
             }
-            nx += posx[j%4];
-            ny += posy[j%4];            
+            
+            else {
+                nx = tmpX;
+                ny = tmpY;
             }
+            
+        }
         return answer;
+        
+        
+        
+        
+        
+        
+//         int[] posy = {1, 0, -1, 0};
+//         int[] posx = {0, 1, 0, -1};
+        
+//         int[][] answer = new int[n][n];
+        
+//         int nx = 0, ny = 0;
+         
+//         for(int i=1, j=0; i<=n*n; i++){
+//             answer[nx][ny] = i;
+            
+//             if(nx+posx[j%4] < 0 || nx+posx[j%4] >= n || ny+posy[j%4] < 0 || ny+posy[j%4] >= n || answer[nx+posx[j%4]][ny+posy[j%4]]!=0){
+//                 j++;
+//             }
+//             nx += posx[j%4];
+//             ny += posy[j%4];            
+//             }
+//         return answer;
         
     }
 }
