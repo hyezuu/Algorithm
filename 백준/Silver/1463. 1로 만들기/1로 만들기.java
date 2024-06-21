758ea2793f669cc4adedcd71dd6b336ef101aacd
hyezuu <147456219+hyezuu@users.noreply.github.com>
2024-06-22 00:18:27 +0900

    [Silver III] Title: 1로 만들기, Time: 124 ms, Memory: 18160 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -15,21 +15,30 @@ class Main {
     }
 
     public int solution(int a) {
-        Queue<Integer> q = new LinkedList<>();
-        int[] visited = new int[a + 1];
-        q.offer(1);
-        visited[1] = 0;
-        while (!q.isEmpty()) {
-            int cur = q.poll();
-            if(cur==a) break;
-            int[] tmp = {cur + 1, cur * 2, cur * 3};
-            for (int i : tmp) {
-                if (i <= a && visited[i] == 0) {
-                    visited[i] = visited[cur] + 1;
-                    q.offer(i);
-                }
-            }
+        int[] arr = new int[a + 1];
+
+        for(int i=2; i<=a; i++){
+            arr[i] = arr[i-1] + 1;
+            if(i%2==0) arr[i] = Math.min(arr[i], arr[i/2]+1);
+            if(i%3==0) arr[i] = Math.min(arr[i], arr[i/3]+1);
         }
-        return visited[a];
+        return arr[a];
+
+//        Queue<Integer> q = new LinkedList<>();
+//        int[] visited = new int[a + 1];
+//        q.offer(1);
+//        visited[1] = 0;
+//        while (!q.isEmpty()) {
+//            int cur = q.poll();
+//            int[] tmp = {cur + 1, cur * 2, cur * 3};
+//            for (int i : tmp) {
+//                if (i <= a && visited[i] == 0) {
+//                    visited[i] = visited[cur] + 1;
+//                    if(i==a) return visited[i];
+//                    q.offer(i);
+//                }
+//            }
+//        }
+//        return visited[a];
     }
 }
