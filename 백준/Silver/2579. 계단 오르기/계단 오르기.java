700626106457477d3bd972bcfa1eb788216d5adf
hyezuu <147456219+hyezuu@users.noreply.github.com>
2024-06-22 23:44:33 +0900

    [Silver III] Title: 계단 오르기, Time: 124 ms, Memory: 14108 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.java
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -19,24 +19,44 @@ class Main {
         System.out.println(main.solution(t, arr));
         br.close();
     }
-
-    public int solution(int t, int[] arr) {
-        if(t==1) return arr[1];
-        //dp[i][j] : 현재까지 j개의 계단을 연속해서 밟고, i번째 계단까지 올라섰을때
-        //점수합의 최댓값. 단 i번째 계단은 반드시 밟아야 함
-        int[][] dp = new int[t+1][3];
-
-        dp[1][1] = arr[1];
-        dp[1][2] = 0;
-        dp[2][1] = arr[2];
-        dp[2][2] = arr[1]+arr[2];
-
-        for(int i=3; i<=t; i++){
-            //현재까지 1개의 계단을 연속해서 밟은 경우 -> 이전에 jump 한 경우 (두칸 전에서의 최댓값)
-            dp[i][1] += Math.max(dp[i-2][1], dp[i-2][2])+arr[i];
-            //현재까지 두개의 계단을 연속해서 밟은경우 -> 이전에 go 한경우 && 이전에 1개까지만 밟은경우
-            dp[i][2] += dp[i-1][1]+arr[i];
+    public int solution(int n, int[] arr) {
+        int sum = 0;
+        if(n<=2) {
+            for(int a : arr){
+                sum+=a;
+            }
+            return sum;
         }
-        return Math.max(dp[t][1], dp[t][2]);
+        int[] dp = new int[n+1];
+        sum = arr[1]+arr[2]+arr[3];
+        dp[1] = arr[1];
+        dp[2] = arr[2];
+        dp[3] = arr[3];
+        for(int i=4; i<=n; i++){
+            sum += arr[i];
+            dp[i] = Math.min(dp[i-2], dp[i-3]) + arr[i];
+        }
+
+        return sum-Math.min(dp[n-1], dp[n-2]);
     }
+
+//    public int solution(int t, int[] arr) {
+//        if(t==1) return arr[1];
+//        //dp[i][j] : 현재까지 j개의 계단을 연속해서 밟고, i번째 계단까지 올라섰을때
+//        //점수합의 최댓값. 단 i번째 계단은 반드시 밟아야 함
+//        int[][] dp = new int[t+1][3];
+//
+//        dp[1][1] = arr[1];
+//        dp[1][2] = 0;
+//        dp[2][1] = arr[2];
+//        dp[2][2] = arr[1]+arr[2];
+//
+//        for(int i=3; i<=t; i++){
+//            //현재까지 1개의 계단을 연속해서 밟은 경우 -> 이전에 jump 한 경우 (두칸 전에서의 최댓값)
+//            dp[i][1] += Math.max(dp[i-2][1], dp[i-2][2])+arr[i];
+//            //현재까지 두개의 계단을 연속해서 밟은경우 -> 이전에 go 한경우 && 이전에 1개까지만 밟은경우
+//            dp[i][2] += dp[i-1][1]+arr[i];
+//        }
+//        return Math.max(dp[t][1], dp[t][2]);
+//    }
 }
