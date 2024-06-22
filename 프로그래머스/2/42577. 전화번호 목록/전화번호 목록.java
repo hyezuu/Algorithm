ae81b52a3b5ec2fe79d82a36e8d8da2ee51a6861
hyezuu <147456219+hyezuu@users.noreply.github.com>
2024-06-23 01:41:35 +0900

    [level 2] Title: 전화번호 목록, Time: 182.54 ms, Memory: 131 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -2,16 +2,16 @@ import java.util.*;
 
 class Solution {
     public boolean solution(String[] phone_book) {
-        // 전화번호 목록을 해시맵에 저장
-        Map<String, Boolean> map = new HashMap<>();
+        // 전화번호 목록을 해시셋에 저장
+        Set<String> set = new HashSet<>();
         for (String number : phone_book) {
-            map.put(number, true);
+            set.add(number);
         }
 
         // 각 전화번호에 대해 접두사 확인
         for (String number : phone_book) {
             for (int i = 1; i < number.length(); i++) {
-                if (map.containsKey(number.substring(0, i))) {
+                if (set.contains(number.substring(0, i))) {
                     return false;
                 }
             }
