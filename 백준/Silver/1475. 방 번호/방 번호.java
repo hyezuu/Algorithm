5f5cb7bf072f910fc6013cde712423f336862d7a
hyezuu <147456219+hyezuu@users.noreply.github.com>
2024-06-22 17:26:56 +0900

    [Silver V] Title: 방 번호, Time: 124 ms, Memory: 14252 KB -BaekjoonHub

--- 백준/Silver/1475. 방 번호/방 번호.java
+++ 백준/Silver/1475. 방 번호/방 번호.java
@@ -1,38 +1,37 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 
-public class Main {
-    public static void main(String[] args) throws Exception {
-        Main T = new Main();
+class Main {
+    public static void main(String[] args) throws IOException {
+
+        Main main = new Main();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
 
-        String a = br.readLine();
+        System.out.print(main.solution(n));
+        br.close();
 
-        bw.write(T.solution(a));
-        bw.flush();
     }
-
-    public String solution(String a) {
-        int[] nums = new int[10];
-        for(char c : a.toCharArray()){
-            int num = c-'0';
-            if(num==9||num==6){
-                if(nums[6]<nums[9]){
-                    nums[6] = nums[6]+1;
-                }
-                else nums[9] = nums[9]+1;
-            }
-            else nums[num] = nums[num]+1;
-        }
+    public int solution(int n) {
+        int[] arr = new int[10];
         int max = 0;
-        for(int i=0; i<10; i++){
-            if(max<nums[i]){
-                max=nums[i];
+
+        while(n>0){
+            int tmp = n%10;
+
+            if(tmp==6||tmp==9){
+                if(arr[6]<arr[9]) arr[6]++;
+                else arr[9]++;
             }
+            else arr[tmp]++;
+            n/=10;
         }
-        return max+"";
+
+        for(int i : arr){
+            max = Math.max(max,i);
+        }
+
+        return max;
     }
 }
