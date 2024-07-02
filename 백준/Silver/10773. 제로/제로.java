0d937e19a1d7ab43c1339edede72a9ef56453239
hyezuu <147456219+hyezuu@users.noreply.github.com>
2024-07-03 00:31:39 +0900

    [Silver IV] Title: 제로, Time: 228 ms, Memory: 23928 KB -BaekjoonHub

--- 백준/Silver/10773. 제로/제로.java
+++ 백준/Silver/10773. 제로/제로.java
@@ -1,36 +1,36 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.*;
 import java.util.*;
 
-public class Main {
-    public String solution(int[] arr) {
-        int answer = 0;
-        Stack<Integer> stack = new Stack<>();
-        for(int a : arr){
-            if(a!=0){
-                stack.push(a);
-            }
-            else stack.pop();
-        }
-
-        for(int a : stack){
-            answer+=a;
-        }
-        return answer+"";
-    }
-
-    public static void main(String[] args) throws Exception {
-        Main T = new Main();
+class Main {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int n = Integer.parseInt(br.readLine());
-        int[] arr = new int[n];
-        for(int i=0; i<n; i++){
+
+        int k = Integer.parseInt(br.readLine());
+        int[] arr = new int[k];
+
+        for(int i=0; i<k; i++) {
             arr[i] = Integer.parseInt(br.readLine());
         }
-        bw.write(T.solution(arr));
-        bw.flush();
+
+        System.out.print(solution(arr));
+        br.close();
+    }
+
+    public static int solution(int[] arr) {
+        int sum = 0;
+        Stack<Integer> stack = new Stack<>();
+
+        for(int num : arr) {
+            if(num == 0) {
+                stack.pop();
+            }
+            else stack.push(num);
+        }
+
+        for(int num : stack){
+            sum += num;
+        }
+
+        return sum;
     }
 }
