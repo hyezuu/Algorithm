bd5a0a7c1bd7d919d98e9f812952efd0c77fac04
hyezuu <147456219+hyezuu@users.noreply.github.com>
2024-07-07 04:11:50 +0900

    [Silver III] Title: N과 M (1), Time: 788 ms, Memory: 44836 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -3,17 +3,16 @@ import java.util.Stack;
 import java.util.StringTokenizer;
 
 class Main {
-    static int N;
-    static int M;
     static Stack<Integer> stack;
     static boolean[] visited;
+    static int N;
+    static int M;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer cond = new StringTokenizer(br.readLine());
         N = Integer.parseInt(cond.nextToken());
         M = Integer.parseInt(cond.nextToken());
-
         stack = new Stack<>();
         visited = new boolean[N + 1];
 
@@ -21,10 +20,12 @@ class Main {
     }
 
     private static void recursion() {
-        if (stack.size() == M) print(stack);
-
+        if (stack.size() == M){
+            print(stack);
+            return;
+        }
         for (int i = 1; i <= N; i++) {
-            if(visited[i]) continue;
+            if (visited[i]) continue;
 
             visited[i] = true;
             stack.push(i);
@@ -36,11 +37,9 @@ class Main {
 
     private static void print(Stack<Integer> stack) {
         StringBuilder sb = new StringBuilder();
-
         for (int a : stack) {
             sb.append(a).append(" ");
         }
-
         System.out.println(sb);
     }
 }
