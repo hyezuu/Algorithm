589f1eed438e3debd58bca5ff1d64ab359e7c415
hyezuu <147456219+hyezuu@users.noreply.github.com>
2024-07-07 03:58:41 +0900

    [Silver III] Title: N과 M (1), Time: 860 ms, Memory: 31940 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,14 +1,12 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.Stack;
 import java.util.StringTokenizer;
 
 class Main {
-    static Stack<Integer> stack;
-    static boolean[] visited;
     static int N;
     static int M;
+    static Stack<Integer> stack;
+    static boolean[] visited;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -16,17 +14,13 @@ class Main {
         N = Integer.parseInt(cond.nextToken());
         M = Integer.parseInt(cond.nextToken());
 
-        solution(N, M);
-    }
-
-    private static void solution(int N, int M) {
         stack = new Stack<>();
         visited = new boolean[N + 1];
 
-        recursion(1);
+        recursion();
     }
 
-    private static void recursion(int pos) {
+    private static void recursion() {
         if (stack.size() == M) print(stack);
 
         for (int i = 1; i <= N; i++) {
@@ -34,10 +28,9 @@ class Main {
 
             visited[i] = true;
             stack.push(i);
-            recursion(i);
+            recursion();
             stack.pop();
             visited[i] = false;
-
         }
     }
 
@@ -47,7 +40,7 @@ class Main {
         for (int a : stack) {
             sb.append(a).append(" ");
         }
+
         System.out.println(sb);
-        ;
     }
 }
