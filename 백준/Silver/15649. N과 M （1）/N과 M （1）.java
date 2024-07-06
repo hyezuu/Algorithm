6d4e87bc4412890e9bf78f21184953159a0c3248
hyezuu <147456219+hyezuu@users.noreply.github.com>
2024-07-07 00:46:49 +0900

    [Silver III] Title: N과 M (1), Time: 924 ms, Memory: 34720 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -5,43 +5,49 @@ import java.util.Stack;
 import java.util.StringTokenizer;
 
 class Main {
+    static Stack<Integer> stack;
     static boolean[] visited;
     static int N;
     static int M;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer cond = new StringTokenizer(br.readLine());
         N = Integer.parseInt(cond.nextToken());
         M = Integer.parseInt(cond.nextToken());
 
-        solution(N,M);
+        solution(N, M);
     }
 
     private static void solution(int N, int M) {
-        Stack<Integer> stack = new Stack<>();
+        stack = new Stack<>();
         visited = new boolean[N + 1];
 
-        recursion(stack);
+        recursion(1);
     }
-    private static void recursion (Stack<Integer> stack){
-        if(stack.size()==M) print(stack);
-        for(int i = 1; i <= N; i++){
-            if(!visited[i]){
-                visited[i] = true;
-                stack.push(i);
-                recursion(stack);
-                stack.pop();
-                visited[i] = false;
-            }
+
+    private static void recursion(int pos) {
+        if (stack.size() == M) print(stack);
+
+        for (int i = 1; i <= N; i++) {
+            if(visited[i]) continue;
+
+            visited[i] = true;
+            stack.push(i);
+            recursion(i);
+            stack.pop();
+            visited[i] = false;
+
         }
     }
 
-    private static void print(Stack<Integer> stack){
+    private static void print(Stack<Integer> stack) {
         StringBuilder sb = new StringBuilder();
 
-        for(int a : stack){
+        for (int a : stack) {
             sb.append(a).append(" ");
         }
-        System.out.println(sb);;
+        System.out.println(sb);
+        ;
     }
 }
