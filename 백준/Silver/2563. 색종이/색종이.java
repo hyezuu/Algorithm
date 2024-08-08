ebcb22c36eb69125939a591136d7e9b50181e6af
hyezuu <147456219+hyezuu@users.noreply.github.com>
2024-08-08 11:43:00 +0900

    [Silver V] Title: 색종이, Time: 104 ms, Memory: 14192 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.java
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -8,33 +8,23 @@ class Main {
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        int[][] matrix = new int[n][2];
-        int[] max = new int[2];
+        boolean[][] area = new boolean[101][101];
+
+        int result = 0;
         for (int i = 0; i < n; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            matrix[i][0] = Integer.parseInt(st.nextToken());
-            matrix[i][1] = Integer.parseInt(st.nextToken());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
 
-            max[0] = Math.max(max[0], matrix[i][0]);
-            max[1] = Math.max(max[1], matrix[i][1]);
-        }
-
-        System.out.println(area(matrix, max));
-    }
-
-    static int area (int[][] matrix, int[] max) {
-        int[][] area = new int[max[0]+10][max[1]+10];
-
-        int cnt = 0;
-        for(int i=0; i<matrix.length; i++) {
-            for(int j=matrix[i][0]; j<matrix[i][0]+10; j++) {
-                for(int k=matrix[i][1]; k<matrix[i][1]+10; k++) {
-                    if(area[j][k] == 1) cnt++;
-                    else area[j][k] = 1;
+            for (int j = x; j < x+10; j++) {
+                for (int k = y; k < y+10; k++) {
+                    if(!area[j][k]){
+                        area[j][k] = true;
+                        result++;
+                    }
                 }
             }
         }
-
-        return matrix.length*100 - cnt;
+        System.out.print(result);
     }
 }
