15eceec0f7386b70574a419541a2341f0b1da985
hyezuu <147456219+hyezuu@users.noreply.github.com>
2024-08-13 01:14:57 +0900

    [Silver III] Title: 수열, Time: 284 ms, Memory: 23792 KB -BaekjoonHub

---
+++ 백준/Silver/2559. 수열/수열.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        int[] nums = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for(int i=0; i<N; i++){
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
+
+        System.out.print(solution(nums, K));
+        br.close();
+    }
+
+    static int solution(int[] nums, int k){
+        int sum = 0;
+        int max = Integer.MIN_VALUE;
+        for(int i=0; i<k; i++){
+            sum+=nums[i];
+        }
+        max = sum;
+
+        for(int i=k; i<nums.length; i++){
+            sum = sum - nums[i-k] + nums[i];
+            max = Math.max(max, sum);
+        }
+
+        return max;
+    }
+}
