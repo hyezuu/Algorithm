ce8e8ddf0957b0ba5d0ce760ca7a2d6be0e907a9
hyezuu <147456219+hyezuu@users.noreply.github.com>
2024-08-16 02:11:48 +0900

    [Gold V] Title: 카드 바꾸기, Time: 208 ms, Memory: 14464 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/25401. 카드 바꾸기/카드 바꾸기.java
+++ 백준/Gold/25401. 카드 바꾸기/카드 바꾸기.java
@@ -20,28 +20,20 @@ class Main {
     }
 
     static int solution(int[] nums) {
-        Set<Integer> set = new HashSet<>();
-        for (int i = 1; i < nums.length; i++) {
-            set.add(nums[i] - nums[i - 1]);
-        }
-        for (int a : set) {
-            count(nums, a, 0, 0);
-        }
-        return min;
-    }
-
-    static void count(int[] nums, int diff, int idx, int cnt) {
-        int[] copy = Arrays.copyOf(nums, nums.length);
-        for (int i = idx; i < nums.length - 1; i++) {
-            if (copy[i + 1] - copy[i] != diff) {
-                cnt++;
-                int tmp = copy[i];
-                copy[i] = copy[i + 1] - diff;
-                count(copy, diff, i+1, cnt);
-                copy[i] = tmp;
-                copy[i + 1] = copy[i] + diff;
+        int min = Integer.MAX_VALUE;
+        for(int i=0; i<nums.length; i++) {
+            for(int j=i+1; j<nums.length; j++) {
+                if((nums[j] - nums[i]) % (j-i) != 0) continue;
+                int diff = (nums[j] - nums[i]) / (j-i);
+                int cnt = 0;
+                for(int k=0; k<nums.length; k++) {
+                    if(nums[k] != (k-i) * diff + nums[i]){
+                        cnt ++;
+                    }
+                }
+                min = Math.min(min, cnt);
             }
         }
-        min = Math.min(min, cnt);
+        return min;
     }
 }
