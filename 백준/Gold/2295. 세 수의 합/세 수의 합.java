714ad453ea1ee1b2b380a2279a86b24b48987622
hyezuu <147456219+hyezuu@users.noreply.github.com>
2024-10-08 09:53:18 +0900

    [Gold IV] Title: 세 수의 합, Time: 304 ms, Memory: 55028 KB -BaekjoonHub

--- 백준/Gold/2295. 세 수의 합/세 수의 합.java
+++ 백준/Gold/2295. 세 수의 합/세 수의 합.java
@@ -18,40 +18,22 @@ class Main {
     public static int solution(int n, int[] nums) {
         Arrays.sort(nums);
 
-        List<Integer> list = new ArrayList<>();
+        Set<Integer> set = new HashSet<>();
 
         for(int i=0; i<n; i++){
             for(int j=0; j<n; j++){
-                list.add(nums[j]+nums[i]);
+                set.add(nums[j]+nums[i]);
             }
         }
-
-        Collections.sort(list);
 
         for (int i = n - 1; i >= 0; i--) {
             for (int j = 0; j < i; j++) {
                 int target = nums[i] - nums[j];
-                if (search(list, target)) {
+                if (set.contains(target)) {
                     return nums[i];
                 }
             }
         }
         return 0;
-    }
-
-    public static boolean search(List<Integer> list, int target) {
-        int lt = 0, rt = list.size() - 1;
-        while (lt < rt) {
-            int mid = (lt + rt) / 2;
-
-            if (list.get(mid) == target) {
-                return true;
-            } else if (list.get(mid) > target) {
-                rt = mid - 1;
-            } else {
-                lt = mid + 1;
-            }
-        }
-        return false;
     }
 }
