d9a89e2c5841d7885dc678311e0ab48e3d08a2b5
hyezuu <147456219+hyezuu@users.noreply.github.com>
2024-10-10 13:38:33 +0900

    [level 1] Title: 옹알이 (2), Time: 11.24 ms, Memory: 86.5 MB -BaekjoonHub

--- 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.java
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.java
@@ -1,22 +1,12 @@
 class Solution {
     public int solution(String[] babbling) {
-        String[] str = {"aya", "ye", "woo", "ma"};
-        int cnt = 0;
-        for(String babble : babbling){
-            String tmp = babble;
-            for(int i=0; i<4; i++){
-               tmp = tmp.replaceAll(str[i], String.valueOf(i));
-            }
-            boolean flag = true;
-            for(int i=1; i<tmp.length(); i++){
-                if(!Character.isDigit(tmp.charAt(i)) || tmp.charAt(i)==tmp.charAt(i-1)){
-                    flag = false;
-                    break;
-                }
-            }
-            if(!Character.isDigit(tmp.charAt(0))) flag = false;
-            if(flag) cnt++;
+        int answer = 0;
+        for(int i=0; i<babbling.length; i++){
+            babbling[i] = babbling[i].replaceAll("ayaaya|yeye|woowoo|mama","1");
+            babbling[i] = babbling[i].replaceAll("aya|ye|woo|ma"," ");
+            babbling[i] = babbling[i].replaceAll(" ","");
+            if(babbling[i].equals("")) answer++;
         }
-        return cnt;
+        return answer;
     }
 }
