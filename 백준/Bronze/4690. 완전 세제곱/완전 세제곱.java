66597ed1123777871fd8bd25d6cb1a3794201e9a
hyezuu <147456219+hyezuu@users.noreply.github.com>
2025-01-06 17:48:26 +0900

    [Bronze III] Title: 완전 세제곱, Time: 184 ms, Memory: 15408 KB -BaekjoonHub

--- 백준/Bronze/4690. 완전 세제곱/완전 세제곱.java
+++ 백준/Bronze/4690. 완전 세제곱/완전 세제곱.java
@@ -3,11 +3,26 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        for(int i=2; i<=100; i++){
-            for(int j=2; j<=100; j++){
-                for(int k=j+1;k<=100 ; k++){
-                    for(int l=k+1; l<=100; l++){
-                        if(i*i*i == j*j*j + k*k*k + l*l*l){
+        int a3 = 0, b3 = 0, c3 = 0, d3 = 0;
+
+        for (int i = 2; i <= 100; i++) {
+            a3 = i * i * i;
+            for (int j = 2; j <= 100; j++) {
+                b3 = j * j * j;
+                if (b3 >= a3) {
+                    break;
+                }
+                for (int k = j + 1; k <= 100; k++) {
+                    c3 = k * k * k;
+                    if (b3 + c3 >= a3) {
+                        break;
+                    }
+                    for (int l = k + 1; l < i; l++) {
+                        d3 = l * l * l;
+                        if (b3 + c3 + d3 > a3) {
+                            break;
+                        }
+                        if (b3 + c3 + d3 == a3) {
                             System.out.printf("Cube = %d, Triple = (%d,%d,%d)", i, j, k, l);
                             System.out.println();
                         }
