13625f0800b449c89fdb1ad482368378466c38e5
hyezuu <147456219+hyezuu@users.noreply.github.com>
2025-01-23 18:02:23 +0900

    [Bronze I] Title: 유레카 이론, Time: 172 ms, Memory: 16960 KB -BaekjoonHub

--- 백준/Bronze/10448. 유레카 이론/유레카 이론.java
+++ 백준/Bronze/10448. 유레카 이론/유레카 이론.java
@@ -1,44 +1,41 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		int T = Integer.parseInt(br.readLine());
-		while(T-- > 0){
-			int tmp = Integer.parseInt(br.readLine());
-			int answer = 0;
+		List<Integer> triangleNumList = new ArrayList<>();
+		int idx = 1;
 
-			outer : for(int i = 1; i < tmp-1; i++){
-				if(!isTriangleNumber(i)) continue;
-				for(int j = 1; j < tmp-1; j++){
-					if(!isTriangleNumber(j)) continue;
-					for(int k = 1; k < tmp-1; k++){
-						if(!isTriangleNumber(k)) continue;
-						if(i+j+k == tmp) {
-							answer = 1;
-							break outer;
-						}
-					}
+		while (true) {
+			int num = idx * (idx + 1) / 2;
+			if (num > 1000) {
+				break;
+			}
+			triangleNumList.add(num);
+			idx++;
+		}
+
+		Set<Integer> triangleNumSet = new HashSet<>();
+
+		for (int i = 0; i < triangleNumList.size(); i++) {
+			for (int j = 0; j < triangleNumList.size(); j++) {
+				for (int k = 0; k < triangleNumList.size(); k++) {
+					triangleNumSet.add(
+						triangleNumList.get(i) + triangleNumList.get(j) + triangleNumList.get(k));
 				}
 			}
-			sb.append(answer).append("\n");
+		}
+		StringBuilder sb = new StringBuilder();
+
+		int T = Integer.parseInt(br.readLine());
+		for (int i = 0; i < T; i++) {
+			int num = Integer.parseInt(br.readLine());
+			sb.append(triangleNumSet.contains(num) ? "1" : "0").append("\n");
 		}
 
 		System.out.print(sb);
-
 	}
 
-	static boolean isTriangleNumber(int n){
-		int sum = 0;
-		for(int i=1; i<=(n/2)+1; i++){
-			sum+=i;
-			if(sum==n) return true;
-		}
-		return false;
-	}
 }
