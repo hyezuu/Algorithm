6b08e192a8ed74c3f40b4d3c44710b3f91d96e80
hyezuu <147456219+hyezuu@users.noreply.github.com>
2025-01-25 19:09:14 +0900

    [Bronze II] Title: 수학은 비대면강의입니다, Time: 124 ms, Memory: 15996 KB -BaekjoonHub

--- 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.java
+++ 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.java
@@ -14,14 +14,10 @@ public class Main {
 		int e = Integer.parseInt(st.nextToken());
 		int f = Integer.parseInt(st.nextToken());
 
-		for(int i=-999; i<=999; i++){
-			for(int j=-999; j<=999; j++){
-				if((a*i + j*b == c) && (d*i + e*j==f)){
-					System.out.print(i+" "+j);
-					break;
-				}
-			}
-		}
+		int y = (c * d - f * a) / (b * d - e * a);
+		int x = (c * e - f * b) / (a * e - b * d);
+
+		System.out.print(x + " " + y);
 
 	}
 }
