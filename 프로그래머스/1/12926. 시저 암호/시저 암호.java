f1361c492a8a2c8dd0921b08acd51d721929eca0
hyezuu <147456219+hyezuu@users.noreply.github.com>
2025-02-26 23:59:35 +0900

    [level 1] Title: 시저 암호, Time: 1.63 ms, Memory: 85.6 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/시저 암호.java
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.java
@@ -1,15 +1,14 @@
 class Solution {
     public String solution(String s, int n) {
         StringBuilder sb = new StringBuilder();
-        
         for(char c : s.toCharArray()){
             if(Character.isAlphabetic(c)){
                 if(Character.isLowerCase(c)){
-                    sb.append((char)((c-'a'+n)%26+'a'));
+                    c = (char)((c-'a'+n)%26+'a');
                 }
-                else sb.append((char)((c-'A'+n)%26+'A'));
+                else c = (char)((c-'A'+n)%26+'A');
             }
-            else sb.append(c);
+            sb.append(c);
         }
         return sb.toString();
     }
