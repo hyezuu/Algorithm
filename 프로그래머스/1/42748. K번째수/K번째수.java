c9a6fd14555c0a53986b404d87f775c85b972e63
hyezuu <147456219+hyezuu@users.noreply.github.com>
2025-03-01 05:27:02 +0900

    [level 1] Title: K번째수, Time: 0.03 ms, Memory: 84.5 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -1,16 +1,35 @@
-import java.util.*;
-
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
         int[] answer = new int[commands.length];
-        int idx = 0;
-        for(int[] com : commands){
-            int len = com[1]-com[0]+1;
-            int[] copy = new int[len];
-            System.arraycopy(array, com[0]-1, copy, 0, len);
-            Arrays.sort(copy);
-            answer[idx++] = copy[com[2]-1];
+    
+        for(int i=0; i<commands.length; i++){
+            int s = commands[i][0]-1;
+            int e = commands[i][1]-1;
+            int k = commands[i][2]-1;
+            
+            int[] tmp = new int[e-s+1];
+            
+            for(int j=s, idx=0; j<=e; j++){
+                tmp[idx++] = array[j];
+            }
+            sort(tmp);
+            answer[i] = tmp[k];
         }
         return answer;
+    }
+    
+    private int[] sort(int[] arr){
+        for(int i=0; i<arr.length-1; i++){
+            int minIdx = i;
+            for(int j=i+1; j<arr.length; j++){
+                if(arr[minIdx] > arr[j]){
+                    minIdx = j;
+                }
+            }
+            int tmp = arr[i];
+            arr[i] = arr[minIdx];
+            arr[minIdx] = tmp;
+        }
+        return arr;
     }
 }
