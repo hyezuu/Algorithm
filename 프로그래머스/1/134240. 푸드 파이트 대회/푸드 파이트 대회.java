bd8bb1e746d3eb42f272d7d162531094b86e3c17
hyezuu <147456219+hyezuu@users.noreply.github.com>
2025-03-04 10:19:31 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 17.55 ms, Memory: 81.4 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -1,13 +1,14 @@
 class Solution {
     public String solution(int[] food) {
         String answer = "0";
-
-        for (int i = food.length - 1; i > 0; i--) {
-            for (int j = 0; j < food[i] / 2; j++) {
-                answer = i + answer + i; 
+        
+        for(int i=food.length-1; i>=0; i--){
+            int tmp = food[i]/2;
+            if(tmp > 0){
+                answer = (i+"").repeat(tmp) + answer + (i+"").repeat(tmp);
             }
         }
-
+        
         return answer;
     }
 }
