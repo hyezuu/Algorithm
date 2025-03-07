11ed3883796f0793e55cf71314cbd0a45049e62f
hyezuu <147456219+hyezuu@users.noreply.github.com>
2025-03-07 10:12:12 +0900

    [level 1] Title: 2016년, Time: 0.04 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/1/12901. 2016년/2016년.java
+++ 프로그래머스/1/12901. 2016년/2016년.java
@@ -1,12 +1,16 @@
 class Solution {
     public String solution(int a, int b) {
-        int[] months = {0,31,29,31,30,31,30,31,31,30,31,30,31};
-        String[] date = {"SUN","MON","TUE","WED","THU","FRI","SAT"};
-        int days = 0;
-        for(int i=0; i<a; i++){
-            days+=months[i];
+        int[] date = {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        String[] days = {"FRI","SAT","SUN","MON","TUE","WED","THU"};
+        
+        int totalDate = 0;
+        
+        for(int i=1; i<a; i++){
+            totalDate += date[i];
         }
-        days+=b;
-        return date[(days+4)%7];
+        
+        totalDate += b;
+        
+        return days[(totalDate-1)%7];
     }
 }
