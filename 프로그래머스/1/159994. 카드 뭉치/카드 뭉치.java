787924629d04a8e8129aa605d4d03db3810d78ca
hyezuu <147456219+hyezuu@users.noreply.github.com>
2025-03-10 10:37:09 +0900

    [level 1] Title: 카드 뭉치, Time: 0.12 ms, Memory: 85.3 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
@@ -2,22 +2,24 @@ import java.util.*;
 
 class Solution {
     public String solution(String[] cards1, String[] cards2, String[] goal) {
-        Queue<String> q1 = new LinkedList<>();
-        Queue<String> q2 = new LinkedList<>();
-        
-        for(String c1 : cards1){
-            q1.offer(c1);
-        }
-        for(String c2 : cards2){
-            q2.offer(c2);
-        }
-        
-        for(String word : goal){
-            if(word.equals(q1.peek())) q1.poll();
-            else if(word.equals(q2.peek())) q2.poll();
-            else return "No";
-        }
-        return "Yes";
-        
-    }
+
+			Queue<String> q1 = new LinkedList<>();
+			Queue<String> q2 = new LinkedList<>();
+
+			for(String card1 : cards1) q1.offer(card1);
+			for(String card2 : cards2) q2.offer(card2);
+			
+			for(String g : goal){
+				if(!q1.isEmpty() && q1.peek().equals(g)){
+					q1.poll();
+					continue;
+				}
+				if(!q2.isEmpty() && q2.peek().equals(g)){
+					q2.poll();
+					continue;
+				}
+				return "No";
+			}
+			return "Yes";
+		}
 }
