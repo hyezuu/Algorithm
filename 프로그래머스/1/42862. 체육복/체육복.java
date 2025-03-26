c65e5cbeca5298ec5ec098c7601db6c7ad225513
hyezuu <147456219+hyezuu@users.noreply.github.com>
2025-03-26 15:40:29 +0900

    [level 1] Title: 체육복, Time: 0.03 ms, Memory: 72.4 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -1,39 +1,36 @@
-import java.util.*;
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
-        int[] students = new int[n+2];
-        for(int i = 1; i <= n; i++) {
-            students[i] = 1;
+        int[] total = new int[n+2];
+        for(int i=1; i<=n; i++){
+            total[i] = 1;
         }
         
-        for(int r : reserve) {
-            students[r]++;
+        for(int l : lost){
+            total[l]--;
         }
         
-        for(int l : lost) {
-            students[l]--;
+        for(int r : reserve){
+            total[r]++;
         }
         
-
-        for(int i = 1; i <= n; i++) {
-            if(students[i] == 0) { 
-                if(students[i-1] > 1) { 
-                    students[i-1]--;
-                    students[i]++;
-                } else if(students[i+1] > 1 && students[i] == 0) { 
-                    students[i+1]--;
-                    students[i]++;
+        for(int i=1; i<=n; i++){
+            if(total[i]==0){
+                if(total[i-1]>1){
+                    total[i-1]--;
+                    total[i]++;
+                }
+                else if(total[i+1]>1){
+                    total[i+1]--;
+                    total[i]++;
                 }
             }
         }
-
-        int answer = 0;
-        for(int i = 1; i <= n; i++) {
-            if(students[i] > 0) {
-                answer++;
-            }
-        }
         
-        return answer;
+        int cnt = 0;
+
+        for(int i=1; i<=n; i++){
+            if(total[i]>0) cnt++;
+        }
+        return cnt;
     }
 }
