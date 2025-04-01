88ccb246f3f51a773d4857b94f3a3896c93c2496
hyezuu <147456219+hyezuu@users.noreply.github.com>
2025-04-01 11:43:42 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 7.47 ms, Memory: 71.6 MB -BaekjoonHub

--- 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.java
+++ 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.java
@@ -4,24 +4,29 @@ class Solution {
     public String solution(String[] survey, int[] choices) {
         Map<Character, Integer> map = new HashMap<>();
         
-        for(int i=0; i<survey.length; i++){
-            int tmp = choices[i];
-            if(tmp==4) continue;
-            char type = survey[i].charAt(tmp/4);
-            int point = tmp/4 == 0 ? 4-(tmp%4) : tmp%4;
-            map.put(type, map.getOrDefault(type,0)+point);
+        String[] cases = {"RT" ,"CF", "JM", "AN"};
+        
+        for(String c : cases){
+            map.put(c.charAt(0), 0);
+            map.put(c.charAt(1), 0);
         }
+        
+        for(int i=0; i<survey.length; i++){
+            if(choices[i] < 4){
+                map.put(survey[i].charAt(0), map.get(survey[i].charAt(0)) +4-choices[i]);
+            }
+            else map.put(survey[i].charAt(1), map.get(survey[i].charAt(1))+choices[i]-4);
+        }
+        
         StringBuilder sb = new StringBuilder();
         
-        String[] types = {"RT", "CF", "JM", "AN"};
-        
-        for(String t : types){
-            int type1 = map.getOrDefault((t.charAt(0)),0);
-            int type2 = map.getOrDefault((t.charAt(1)),0);
-            
-            if(type1 >= type2) sb.append(t.charAt(0));
-            else sb.append(t.charAt(1));
+        for(String cs : cases){
+            if(map.get(cs.charAt(0)) >= map.get(cs.charAt(1))){
+                sb.append(cs.charAt(0));
+            }
+            else sb.append(cs.charAt(1));
         }
+        
         return sb.toString();
     }
 }
