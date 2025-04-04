2e690ae9932fec700d75124d206434e9536946ff
hyezuu <147456219+hyezuu@users.noreply.github.com>
2025-04-04 10:50:53 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 0.79 ms, Memory: 84.7 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
@@ -2,41 +2,37 @@ import java.util.*;
 
 class Solution {
     public List<Integer> solution(String today, String[] terms, String[] privacies) {
-        int year = Integer.parseInt(today.substring(0, 4));
-        int month = Integer.parseInt(today.substring(5, 7));
-        int date = Integer.parseInt(today.substring(8, 10));
-        
         Map<Character, Integer> map = new HashMap<>();
-        
+
         for(String term : terms){
             map.put(term.charAt(0), Integer.parseInt(term.substring(2)));
         }
-        
+
         List<Integer> list = new ArrayList<>();
-        
+
         int idx = 1;
-        
+
         for(String privacy : privacies){
-            int tempYear = Integer.parseInt(privacy.substring(0, 4));
-            int tempMonth = Integer.parseInt(privacy.substring(5, 7));
-            int tempDate = Integer.parseInt(privacy.substring(8, 10));
-            
             int addedDate = 28*(map.get(privacy.charAt(11)));
-            
-            int totalDate = getTotalDate(year, month, date);
-            int expirationDate = getTotalDate(tempYear, tempMonth, tempDate) + addedDate;
-            
+
+            int totalDate = getTotalDate(today);
+            int expirationDate = getTotalDate(privacy) + addedDate;
+
             if(totalDate >= expirationDate){
                 list.add(idx);
             }
             idx++;
         }
-        
+
         return list;
-        
+
     }
-    
-    private int getTotalDate(int year, int month, int date){
+
+    private int getTotalDate(String day){
+        int year = Integer.parseInt(day.substring(0, 4));
+        int month = Integer.parseInt(day.substring(5, 7));
+        int date = Integer.parseInt(day.substring(8, 10));
+        
         return date + month*28 + (year-2000)*(28*12);
     }
 }
