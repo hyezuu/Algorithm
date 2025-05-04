16356194954ec43c90b3c9b9e849af6d997abfca
hyezuu <147456219+hyezuu@users.noreply.github.com>
2025-05-04 11:58:43 +0900

    [level 2] Title: 피보나치 수, Time: 2.35 ms, Memory: 69.1 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
@@ -1,13 +1,14 @@
 class Solution {
     public int solution(int n) {
-        int[] arr = new int[n+1];
-        for(int i=0; i<=n; i++){
-            if(i<=1) arr[i] = i;
-            else {
-                arr[i] = (arr[i-2]+arr[i-1])%1234567;
-            }
+        int[] fibo = new int[n+1];
+        
+        fibo[0] = 0;
+        fibo[1] = 1;
+        
+        for(int i=2; i<=n; i++){
+            fibo[i] = (fibo[i-1]+fibo[i-2])%1234567;
         }
-
-        return arr[n];
+        
+        return fibo[n];
     }
 }
