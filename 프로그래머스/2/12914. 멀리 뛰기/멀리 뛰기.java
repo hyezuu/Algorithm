b98ae4b18643f6127617f1a6e213ebbbd742d4f4
hyezuu <147456219+hyezuu@users.noreply.github.com>
2025-05-10 15:31:10 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.11 ms, Memory: 84 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
@@ -1,14 +1,11 @@
 class Solution {
-    public long solution(int n) {
-        int[] arr = new int[2001];
-        arr[1] = 1;
-        arr[2] = 2;
-        arr[3] = 3;
-        
-        for(int i=4; i<=n; i++){
-            arr[i] = (arr[i-1] + arr[i-2])%1234567;
+    public int solution(int n) {
+        int[]arr=new int[n+1];
+        arr[0]=1;
+        arr[1]=1;
+        for(int i=2;i<n+1;i++){
+            arr[i]=(arr[i-1]+arr[i-2])%1234567;
         }
-        
         return arr[n];
     }
 }
