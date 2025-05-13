68e31cad4cb965071b2698ba417fa33710cb7d59
hyezuu <147456219+hyezuu@users.noreply.github.com>
2025-05-13 11:29:46 +0900

    [level 2] Title: H-Index, Time: 0.79 ms, Memory: 90.8 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -4,12 +4,12 @@ class Solution {
     public int solution(int[] citations) {
         Arrays.sort(citations);
         int cnt = 0;
-        int max = 0;
-        for(int i=citations.length-1; i>=0; i--){
+        int max = Integer.MIN_VALUE;
+        for(int i=citations.length-1 ; i>=0 ; i--){
             cnt++;
-            max = Math.max(Math.min(cnt, citations[i]),max);
-        }
+            max = Math.max(max, Math.min(cnt, citations[i]));
+        }  
+        //6 5 3 1 0
         return max;
     }
-        
 }
