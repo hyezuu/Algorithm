165dff61dc0b9a83a2cb6d19243f4a9936becaf3
hyezuu <147456219+hyezuu@users.noreply.github.com>
2025-05-19 23:53:12 +0900

    [Silver I] Title: 꿈틀꿈틀 호석 애벌레 - 기능성, Time: 124 ms, Memory: 14176 KB, Score: 39 point -BaekjoonHub

--- 백준/Silver/20167. 꿈틀꿈틀 호석 애벌레 － 기능성/꿈틀꿈틀 호석 애벌레 － 기능성.java
+++ 백준/Silver/20167. 꿈틀꿈틀 호석 애벌레 － 기능성/꿈틀꿈틀 호석 애벌레 － 기능성.java
@@ -14,67 +14,39 @@ public class Main {
 
     st = new StringTokenizer(br.readLine());
 
-    int[] branch = new int[N + 1];
+    int[] energy = new int[N];
 
-    for (int i = 1; i <= N; i++) {
-      branch[i] = Integer.parseInt(st.nextToken());
+    for (int i = 0; i <N; i++) {
+      energy[i] = Integer.parseInt(st.nextToken());
     }
 
-    System.out.println(dp(N, K, branch));
+    System.out.println(solve(N, K, energy));
   }
 
-  static long dp(int N, int K, int[] branch) {
-    int[][] dp = new int[2][N + 1]; // dp[0][i]는 현재 합, dp[1][i]는 시작점
-    int[] max = new int[N + 1];     // 최대 탈피 에너지
+  static int solve(int N, int K, int[] energy) {
+    int[] value = new int[N+1];
+    int[] size = new int[N+1];
 
-    for (int i = 1; i <= N; i++) {
-      // 기본적으로 이전 상태 가져오기
-      max[i] = max[i-1];
+    int left = 0;
+    int right = 0;
+    int sum = energy[0];
 
-      // 현재 먹이를 시작점으로 하는 새로운 연속 구간 시작
-      int currentSum = branch[i];
-      int startIdx = i;
-
-      // 이전에 구간이 진행 중이었다면 해당 구간에 현재 먹이 추가
-      if (dp[0][i-1] > 0) {
-        int prevSum = dp[0][i-1];
-        int prevStart = dp[1][i-1];
-
-        // 이전 구간에 현재 먹이 추가
-        int newSum = prevSum + branch[i];
-
-        // 합이 K 이상이면 탈피 에너지 획득하고 구간 종료
-        if (newSum >= K) {
-          max[i] = Math.max(max[i], max[i-1] + newSum - K);
-          dp[0][i] = 0; // 구간 종료
-          dp[1][i] = 0;
-          continue;
-        } else {
-          // 이전 구간 이어서 진행
-          currentSum = newSum;
-          startIdx = prevStart;
-        }
-      }
-
-      // 현재 먹이만으로 K 이상이면 탈피 에너지 획득하고 구간 종료
-      if (currentSum >= K) {
-        max[i] = Math.max(max[i], max[i-1] + currentSum - K);
-        dp[0][i] = 0;
-        dp[1][i] = 0;
+    while(left <= right) {
+      if(sum >= K || right == N-1) {
+        size[left] = right - left + 1;
+        value[left] = Math.max(0, sum - K);
+        if(size[left] == 1 && right != N-1) sum += energy[++right];
+        sum -= energy[left++];
       } else {
-        // 구간 정보 저장하고 계속 진행
-        dp[0][i] = currentSum;
-        dp[1][i] = startIdx;
-
-        // 현재 먹이를 건너뛰는 선택도 고려
-        if (max[i-1] > max[i]) {
-          max[i] = max[i-1];
-          dp[0][i] = 0;
-          dp[1][i] = 0;
-        }
+        sum += energy[++right];
       }
     }
 
-    return max[N];
+    int[] dp = new int[N+1];
+
+    for(int i = N-1; i >= 0; i--) {
+      dp[i] = Math.max(dp[i+1], dp[i+size[i]] + value[i]);
+    }
+    return dp[0];
   }
 }
