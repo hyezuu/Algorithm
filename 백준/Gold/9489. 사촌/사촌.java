8c5a72eeeac710f60455465fe7b915fb49b660e5
hyezuu <147456219+hyezuu@users.noreply.github.com>
2025-05-20 19:05:47 +0900

    [Gold IV] Title: 사촌, Time: 604 ms, Memory: 151184 KB -BaekjoonHub

--- 백준/Gold/9489. 사촌/사촌.java
+++ 백준/Gold/9489. 사촌/사촌.java
@@ -1,13 +1,11 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
-
   static int[] arr;
+  static int[] parent;
   static int n, k;
   static StringBuilder sb;
   static final String EXIT = "0 0";
@@ -24,80 +22,39 @@ public class Main {
       k = Integer.parseInt(st.nextToken());
       st = new StringTokenizer(br.readLine());
       arr = new int[n];
+      parent = new int[n];
       for (int i = 0; i < n; i++) {
         arr[i] = Integer.parseInt(st.nextToken());
       }
-      solve(k);
+      solve();
       line = br.readLine();
     }
     System.out.println(sb.toString());
   }
 
-  public static void solve(int k) {
-    if (k == arr[0]) {
-      sb.append("0\n");
-      return;
-    }
-    List<List<List<Integer>>> list = new ArrayList<>();
-
-    list.add(new ArrayList<>());
-    int level = 0;
-    List<Integer> currentGroup = new ArrayList<>();
-    currentGroup.add(arr[0]);
-
-    int kLevel = -1;
-    int kGroupIdx = -1;
-    int idx = 0;
-    int parentSize = 1;
-    int childSize = 1;
-
-    for (int i = 1; i < n; i++) {
-      if (arr[i] > arr[i - 1] + 1) {
-        list.get(level).add(currentGroup);
-        currentGroup = new ArrayList<>();
+  public static void solve() {
+    parent[0] = -1;
+    int target = 0;
+    int idx = -1;
+    for(int i = 1; i < n; i++) {
+      if(arr[i]==k) target = i;
+      if(arr[i] > arr[i-1]+1){
         idx++;
-        if (i == 1 || (level > 0 && parentSize == list.get(level).size())) {
-          parentSize = childSize;
-          childSize = 0;
-          list.add(new ArrayList<>());
-          idx = 0;
-          level++;
-        }
       }
-      childSize++;
-      currentGroup.add(arr[i]);
-
-      if (arr[i] == k) {
-        kLevel = level;
-        kGroupIdx = idx;
-      }
+      parent[i] = idx;
     }
-    if (!currentGroup.isEmpty()) {
-      list.get(level).add(currentGroup);
-    }
+    int cnt = 0;
 
-    if (kLevel < 2) {
-      sb.append("0\n");
+    if(parent[target] == 0){
+      sb.append(cnt).append("\n");
       return;
     }
 
-    List<List<Integer>> parentLevel = list.get(kLevel-1);
-    int start = 0, end = 0;
-    int answer = 0;
-    for (List<Integer> parentGroup : parentLevel) {
-      end += parentGroup.size();
-      if (start <= kGroupIdx && kGroupIdx < end) {
-        List<List<Integer>> childLevel = list.get(kLevel);
-        for (int i = start; i < Math.min(end, childLevel.size()); i++) {
-          if (i != kGroupIdx) {
-            answer += childLevel.get(i).size();
-          }
-        }
-        break;
-      }
-      start = end;
+    for(int i=1; i<n; i++){
+      if(parent[i] != parent[target] && parent[parent[i]] == parent[parent[target]])
+        cnt++;
     }
 
-    sb.append(answer).append("\n");
+    sb.append(cnt).append("\n");
   }
 }
