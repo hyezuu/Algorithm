48bc6e07c2211ee51be2fcd3667f698eb6a63600
hyezuu <147456219+hyezuu@users.noreply.github.com>
2025-05-22 12:39:46 +0900

    [level 2] Title: 기능개발, Time: 0.28 ms, Memory: 79.8 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -2,35 +2,31 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-        Queue<int[]> q = new LinkedList<>();
-        Map<Integer, Integer> map = new LinkedHashMap<>(); 
-        boolean[] chk = new boolean[progresses.length+1];
- 
+        Queue<Integer> q= new LinkedList<>();
+        List<Integer> list = new ArrayList<>();
+        
         for(int i=0; i<progresses.length; i++){
-            int tmp = (int)Math.ceil((100.0 - progresses[i]) / speeds[i]);
-            q.offer(new int[]{i+1, tmp, 0});
+            int tmp = (100-progresses[i])/speeds[i] + ((100-progresses[i])%speeds[i]==0? 0:1);
+            q.offer(tmp);
         }
-        
-        chk[0] = true;
-        
+
         while(!q.isEmpty()){
-            int[] process = q.poll();
-            int chkIdx = process[0];
-            int date = process[1];
-            int cnt = process[2];
+            int tmp = q.poll();
+            int cnt = 1;
             
-            if(chk[chkIdx-1] && date<=0){
-                map.put(cnt, map.getOrDefault(cnt, 0)+1);
-                chk[chkIdx] = true;
+            while(!q.isEmpty()){
+                if(q.peek()<=tmp){
+                    cnt++;
+                    q.poll();
+                }
+                else break;
             }
-            
-            else q.offer(new int[]{chkIdx, date-1, cnt+1});
+            list.add(cnt);
         }
-        
-        int[] answer = new int[map.size()];
         int i=0;
-        for(int key : map.keySet()){
-            answer[i++] = map.get(key);
+        int[] answer = new int[list.size()];
+        for(int a : list){
+            answer[i++] = a;
         }
         return answer;
     }
