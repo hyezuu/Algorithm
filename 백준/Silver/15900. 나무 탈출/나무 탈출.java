d2fddd7b521d7aa0acea45050ad1c69bf3781db9
hyezuu <147456219+hyezuu@users.noreply.github.com>
2025-05-24 12:59:59 +0900

    [Silver I] Title: 나무 탈출, Time: 912 ms, Memory: 220820 KB -BaekjoonHub

--- 백준/Silver/15900. 나무 탈출/나무 탈출.java
+++ 백준/Silver/15900. 나무 탈출/나무 탈출.java
@@ -9,17 +9,17 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-  static List<List<Integer>> tree;
-  static boolean[] visited;
+  static List<Integer>[] tree;
 
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
     int n = Integer.parseInt(br.readLine());
 
-    tree = new ArrayList<>();
-    for (int i = 0; i <= n; i++) {
-      tree.add(new ArrayList<>());
+    tree = new List[n + 1];
+
+    for(int i = 1; i <= n; i++) {
+      tree[i] = new ArrayList<>();
     }
 
     for (int i = 0; i < n - 1; i++) {
@@ -28,8 +28,8 @@ public class Main {
       int x = Integer.parseInt(st.nextToken());
       int y = Integer.parseInt(st.nextToken());
 
-      tree.get(x).add(y);
-      tree.get(y).add(x);
+      tree[x].add(y);
+      tree[y].add(x);
     }
 
     System.out.println(bfs(n)%2==1 ? "Yes" : "No");
@@ -48,7 +48,7 @@ public class Main {
       int parent = node[0];
       int depth = node[1];
       boolean isLeaf = true;
-      for(Integer child : tree.get(parent)) {
+      for(Integer child : tree[parent]) {
         if(!visited[child]) {
           isLeaf = false;
           visited[child] = true;
