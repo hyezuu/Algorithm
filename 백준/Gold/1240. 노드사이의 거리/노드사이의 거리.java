21125c8ffdbd7a6a79e26b63a08df87151b73d1a
hyezuu <147456219+hyezuu@users.noreply.github.com>
2025-05-31 11:08:28 +0900

    [Gold V] Title: 노드사이의 거리, Time: 284 ms, Memory: 54712 KB -BaekjoonHub

---
+++ 백준/Gold/1240. 노드사이의 거리/노드사이의 거리.java
@@ -0,0 +1,69 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+  static int N, M;
+  static List<int[]>[] tree;
+  static StringBuilder sb;
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+
+    N = Integer.parseInt(st.nextToken());
+    M = Integer.parseInt(st.nextToken());
+
+    tree = new List[N+1];
+
+    for(int i = 0; i <= N; i++){
+      tree[i] = new ArrayList<>();
+    }
+
+    for(int i = 0; i < N-1; i++){
+      st = new StringTokenizer(br.readLine());
+      int a = Integer.parseInt(st.nextToken());
+      int b = Integer.parseInt(st.nextToken());
+      int d = Integer.parseInt(st.nextToken());
+      tree[a].add(new int[]{b, d});
+      tree[b].add(new int[]{a, d});
+    }
+
+    sb = new StringBuilder();
+
+    for(int i = 0; i<M; i++){
+      st = new StringTokenizer(br.readLine());
+      int from = Integer.parseInt(st.nextToken());
+      int to = Integer.parseInt(st.nextToken());
+
+      solve(from, to);
+    }
+    System.out.println(sb.toString());
+  }
+
+  static void solve(int from, int to) {
+    Queue<int[]> q = new LinkedList<>();
+    q.offer(new int[]{from, 0});
+    boolean[] visited = new boolean[N+1];
+    visited[from] = true;
+
+    while(!q.isEmpty()){
+      int[] current = q.poll();
+      int node = current[0];
+      int cost = current[1];
+      if(node == to) {
+        sb.append(cost).append("\n");
+        return;
+      }
+
+      for(int[] next : tree[node]){
+        int nextNode = next[0];
+        int nextCost = next[1];
+
+        if(!visited[nextNode]){
+          visited[nextNode] = true;
+          q.offer(new int[]{nextNode, cost + nextCost});
+        }
+      }
+    }
+  }
+}
