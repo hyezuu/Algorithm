566d19a8c96730006b5e78828f453c10b861d9b3
hyezuu <147456219+hyezuu@users.noreply.github.com>
2025-06-02 12:58:48 +0900

    [Gold V] Title: 빌런 호석, Time: 132 ms, Memory: 14284 KB -BaekjoonHub

--- 백준/Gold/22251. 빌런 호석/빌런 호석.java
+++ 백준/Gold/22251. 빌런 호석/빌런 호석.java
@@ -3,11 +3,13 @@ import java.util.*;
 
 public class Main {
 
-  static int N, K;
+  static int N, K, P, X;
   static int[] segments = {
-      0b1111110, 0b0011000, 0b0110111, 0b0111101, 0b1011001, 0b1101101,
-      0b1101111, 0b0111000, 0b1111111, 0b1111101
+      0b1111110, 0b0011000, 0b0110111, 0b0111101, 0b1011001,
+      0b1101101, 0b1101111, 0b0111000, 0b1111111, 0b1111101
   };
+  static int[] xSegments;
+  static int cnt = 0;
 
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -15,36 +17,39 @@ public class Main {
 
     N = Integer.parseInt(st.nextToken()); // 최대 층 수, 9층..
     K = Integer.parseInt(st.nextToken()); // 최대 보여질 숫자 수 -> 2면 01, 02
-    int P = Integer.parseInt(st.nextToken()); // 반전시킬 최대 LED 수
-    int X = Integer.parseInt(st.nextToken()); // 실제 멈취있는 층
+    P = Integer.parseInt(st.nextToken()); // 반전시킬 최대 LED 수
+    X = Integer.parseInt(st.nextToken()); // 실제 멈취있는 층
 
-    solve(P, X);
+    solve();
   }
 
-  static void solve(int p, int x) {
-    int cnt = 0;
-
-    for(int i = 1; i <= N; i++) {
-      if(i == x) continue;
-
-      int needed = 0;
-      int tempX = x, tempI = i;
-
-      // 각 자릿수별로 비교
-      for(int pos = 0; pos < K; pos++) {
-        int digitX = tempX % 10;
-        int digitI = tempI % 10;
-
-        needed += Integer.bitCount(segments[digitX] ^ segments[digitI]);
-        if(needed > p) break; // 조기 종료
-
-        tempX /= 10;
-        tempI /= 10;
-      }
-
-      if(needed <= p) cnt++;
+  static void solve() {
+    xSegments = new int[K];
+    Arrays.fill(xSegments, 0b1111110);
+    int tmp = X;
+    int idx = K-1;
+    while (tmp > 0){
+      xSegments[idx--] = segments[tmp % 10];
+      tmp /= 10;
     }
 
+    dfs(P, 0, 0);
     System.out.println(cnt);
+  }
+
+  static void dfs(int remain, int pos, int current) {
+    if(pos == K) {
+      if(current >= 1 && current <= N && current != X) {
+        cnt++;
+      }
+      return;
+    }
+
+    for(int i=0; i<10; i++){
+      int needed = Integer.bitCount(xSegments[pos] ^ segments[i]);
+
+      if(needed > remain) continue;
+      dfs(remain - needed, pos+1, current*10 + i);
+    }
   }
 }
