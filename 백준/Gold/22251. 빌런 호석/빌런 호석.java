6b330c8ed587246d625926a8f4b8e3f5954038bd
hyezuu <147456219+hyezuu@users.noreply.github.com>
2025-06-02 11:56:09 +0900

    [Gold V] Title: 빌런 호석, Time: 144 ms, Memory: 14292 KB -BaekjoonHub

--- 백준/Gold/22251. 빌런 호석/빌런 호석.java
+++ 백준/Gold/22251. 빌런 호석/빌런 호석.java
@@ -5,16 +5,8 @@ public class Main {
 
   static int N, K;
   static int[] segments = {
-      0b1111110,
-      0b0011000,
-      0b0110111,
-      0b0111101,
-      0b1011001,
-      0b1101101,
-      0b1101111,
-      0b0111000,
-      0b1111111,
-      0b1111101
+      0b1111110, 0b0011000, 0b0110111, 0b0111101, 0b1011001, 0b1101101,
+      0b1101111, 0b0111000, 0b1111111, 0b1111101
   };
 
   public static void main(String[] args) throws IOException {
@@ -29,40 +21,30 @@ public class Main {
     solve(P, X);
   }
 
-  static void solve(int p, int x){
-    int[] floor = toSegments(x);
+  static void solve(int p, int x) {
     int cnt = 0;
-    //층수 다돔
-    outer : for(int i = 1; i<= N; i++){
+
+    for(int i = 1; i <= N; i++) {
       if(i == x) continue;
 
-      int[] targetFloor = toSegments(i);
       int needed = 0;
+      int tempX = x, tempI = i;
 
-      //반전할 비트수 다더하기
-      for(int j=0; j<K; j++){
-        needed += Integer.bitCount(floor[j] ^ targetFloor[j]);
-        if(needed > p) continue outer;
+      // 각 자릿수별로 비교
+      for(int pos = 0; pos < K; pos++) {
+        int digitX = tempX % 10;
+        int digitI = tempI % 10;
+
+        needed += Integer.bitCount(segments[digitX] ^ segments[digitI]);
+        if(needed > p) break; // 조기 종료
+
+        tempX /= 10;
+        tempI /= 10;
       }
-      
-      cnt++;
+
+      if(needed <= p) cnt++;
     }
 
     System.out.println(cnt);
-  }
-
-  //숫자 층을 비트 배열로바꿔줌
-  static int[] toSegments(int num) {
-    int[] arr = new int[K];
-    //0으로 초기화
-    Arrays.fill(arr, 0b1111110);
-    int i = K - 1;
-    int tmp = num;
-    while (tmp > 0) {
-      //뒷자리부터 채움(각자릿수)
-      arr[i--] = segments[tmp % 10];
-      tmp /= 10;
-    }
-    return arr;
   }
 }
