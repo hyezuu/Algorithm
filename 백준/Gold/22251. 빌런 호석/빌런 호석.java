24e2a5f0da6baa0bc284405e1f138a3ef7ac11c7
hyezuu <147456219+hyezuu@users.noreply.github.com>
2025-06-02 11:48:11 +0900

    [Gold V] Title: 빌런 호석, Time: 220 ms, Memory: 53248 KB -BaekjoonHub

---
+++ 백준/Gold/22251. 빌런 호석/빌런 호석.java
@@ -0,0 +1,64 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+  static int N, K;
+  static int[] segments = {
+      0b1111110,
+      0b0011000,
+      0b0110111,
+      0b0111101,
+      0b1011001,
+      0b1101101,
+      0b1101111,
+      0b0111000,
+      0b1111111,
+      0b1111101
+  };
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+
+    N = Integer.parseInt(st.nextToken()); // 최대 층 수, 9층..
+    K = Integer.parseInt(st.nextToken()); // 최대 보여질 숫자 수 -> 2면 01, 02
+    int P = Integer.parseInt(st.nextToken()); // 반전시킬 최대 LED 수
+    int X = Integer.parseInt(st.nextToken()); // 실제 멈취있는 층
+
+    solve(P, X);
+  }
+
+  static void solve(int p, int x){
+    int[] floor = toSegments(x);
+    int cnt = 0;
+    for(int i = 1; i<= N; i++){
+      if(i == x) continue;
+
+      int[] targetFloor = toSegments(i);
+      int needed = 0;
+
+      for(int j=0; j<K; j++){
+        needed += Integer.bitCount(floor[j] ^ targetFloor[j]);
+      }
+
+      if(needed <= p) {
+        cnt++;
+      }
+    }
+
+    System.out.println(cnt);
+  }
+
+  static int[] toSegments(int num) {
+    int[] arr = new int[K];
+    Arrays.fill(arr, 0b1111110);
+    int i = K - 1;
+    int tmp = num;
+    while (tmp > 0) {
+      arr[i--] = segments[tmp % 10];
+      tmp /= 10;
+    }
+    return arr;
+  }
+}
