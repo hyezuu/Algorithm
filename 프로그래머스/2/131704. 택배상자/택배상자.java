255034b5bcef31ceb02e3073cde500e5304ad819
hyezuu <147456219+hyezuu@users.noreply.github.com>
2025-06-10 12:13:06 +0900

    [level 2] Title: 택배상자, Time: 43.79 ms, Memory: 132 MB -BaekjoonHub

--- 프로그래머스/2/131704. 택배상자/택배상자.java
+++ 프로그래머스/2/131704. 택배상자/택배상자.java
@@ -3,17 +3,21 @@ import java.util.*;
 class Solution {
     public int solution(int[] order) {
         Stack<Integer> stack = new Stack<>();
+        int curBox = 1;
         int cnt = 0;
-        int box = 1;
-        for(int i : order){
-            while(box<=i){
-                stack.push(box++);
+        for(int box : order){
+            //빼야하는 상자가 더 큰 경우
+            while(box >= curBox){
+                //보조컨테이너에 넣음
+                stack.push(curBox++);
             }
-            if(stack.pop()==i){
+            if(!stack.isEmpty() && stack.peek() == box){
+                stack.pop();
                 cnt++;
             }
-            else break;
+            else return cnt;
         }
+        
         return cnt;
     }
 }
