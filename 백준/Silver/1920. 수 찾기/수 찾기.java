81bc4914cc4548b78d05735c9801e8cdcc83d928
hyezuu <147456219+hyezuu@users.noreply.github.com>
2025-06-11 11:31:35 +0900

    [Silver IV] Title: 수 찾기, Time: 540 ms, Memory: 49320 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,50 +1,39 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.StringTokenizer;
 
-class Main {
+public class Main {
+  static Set<Integer> set;
+  static StringBuilder sb;
 
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int n = Integer.parseInt(br.readLine());
-        int[] nums = new int[n];
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < n; i++) {
-            nums[i] = Integer.parseInt(st.nextToken());
-        }
+    int N = Integer.parseInt(br.readLine());
 
-        int m = Integer.parseInt(br.readLine());
-        int[] targets = new int[m];
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < m; i++) {
-            targets[i] = Integer.parseInt(st.nextToken());
-        }
-        int[] answer = solution(n, nums, m, targets);
-        for (int i = 0; i < m; i++) {
-            System.out.println(answer[i]);
-        }
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    set = new HashSet<>();
+
+    for(int i=0; i<N; i++){
+      set.add(Integer.parseInt(st.nextToken()));
     }
 
-    public static int[] solution(int n, int[] nums, int m, int[] targets) {
-        Arrays.sort(nums);
-        int[] answer = new int[m];
+    int M = Integer.parseInt(br.readLine());
+    st = new StringTokenizer(br.readLine());
 
-        for (int i = 0; i < m; i++) {
-            int lt = 0, rt = n - 1;
-            while (lt <= rt) {
-                int mid = (lt + rt) / 2;
-                if (targets[i] == nums[mid]) {
-                    answer[i] = 1;
-                    break;
-                } else if (targets[i] < nums[mid]) {
-                    rt = mid - 1;
-                } else {
-                    lt = mid + 1;
-                }
-            }
-        }
-        return answer;
+    sb = new StringBuilder();
+
+    for(int i=0; i<M; i++){
+      search(Integer.parseInt(st.nextToken()));
     }
+
+    System.out.println(sb.toString());
+  }
+
+  static void search(int target){
+    sb.append(set.contains(target) ? "1" : "0").append("\n");
+  }
 }
