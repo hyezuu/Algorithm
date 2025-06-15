7067b825e1b54feea4cf7a73aa7b792d83404563
hyezuu <147456219+hyezuu@users.noreply.github.com>
2025-06-15 17:39:53 +0900

    [level 2] Title: 롤케이크 자르기, Time: 25.79 ms, Memory: 148 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.java
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.java
@@ -1,30 +1,17 @@
-import java.util.*;
-
 class Solution {
-    public int solution(int[] toppings) {
-        int cnt = 0;
-        Map<Integer, Integer> map = new HashMap<>();
-        for(int topping : toppings){
-            map.put(topping, map.getOrDefault(topping, 0) + 1);
+    public int solution(int[] topping) {
+        int answer = 0;
+        int[] left = new int[10001], right = new int[10001];
+        int ls = 0, rs = 0;
+        for(var i : topping) right[i]++;
+        for(var i : right) rs += i > 0 ? 1 : 0;
+        for(var i : topping) {
+            right[i]--;
+            if (right[i] == 0) rs--;
+            if (left[i] == 0) ls++;
+            left[i]++;
+            if (rs == ls) answer++;
         }
-        int totalCnt = map.size();
-        
-        Set<Integer> set = new HashSet<>();
-        
-        int ob = 0;
-        int yb = totalCnt;
-        
-        for(int topping : toppings){
-            if(set.add(topping))ob++;
-            map.put(topping, map.get(topping) - 1);
-            if(map.get(topping) == 0){
-                map.remove(topping);
-                yb--;
-            }
-            if(ob==yb) cnt++;
-            if(ob>yb)break;
-        }
-        
-        return cnt;
+        return answer;
     }
 }
