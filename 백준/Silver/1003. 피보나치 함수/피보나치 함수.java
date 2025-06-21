d6e2b2cc18375ba810c125896587e046d187db53
hyezuu <147456219+hyezuu@users.noreply.github.com>
2025-06-21 10:21:11 +0900

    [Silver III] Title: 피보나치 함수, Time: 104 ms, Memory: 14096 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Main {
 
@@ -19,7 +18,7 @@ public class Main {
       sb.append(dp[N][0])
           .append(" ")
           .append(dp[N][1])
-          .append("\n");
+          .append('\n');
     }
 
     System.out.print(sb);
@@ -36,5 +35,4 @@ public class Main {
       dp[i][1] = dp[i-1][1] + dp[i-2][1];
     }
   }
-
 }
