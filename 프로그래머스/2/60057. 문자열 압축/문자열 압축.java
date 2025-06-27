50af6cd998df8c7d77479f8c5ac322d17b80822d
hyezuu <147456219+hyezuu@users.noreply.github.com>
2025-06-27 11:51:30 +0900

    [level 2] Title: 문자열 압축, Time: 2.00 ms, Memory: 91.7 MB -BaekjoonHub

--- 프로그래머스/2/60057. 문자열 압축/문자열 압축.java
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.java
@@ -3,6 +3,7 @@ import java.util.*;
 class Solution {
     static String str;
     static int min;
+    
     public int solution(String s) {
         str = s;
         min = s.length();
@@ -17,23 +18,23 @@ class Solution {
         int len = 0;
         //10개면 - 4개의 토큰
         int tokens = str.length()/c + (str.length()%c == 0 ? 0 : 1);
-        
         String prev = str.substring(0, c);
         int cnt = 1;
+        
         for(int i=1; i<tokens; i++){
             String current = subs(i, c);
             if(prev.equals(current)){
                 cnt++;
             }
             else {
-                len += c + (cnt == 1 ? 0 : (cnt+"").length());
+                len += c + (cnt == 1 ? 0 : (int)Math.log10(cnt)+1);
                 cnt = 1;
                 prev = current;
                 if(len>min) break;
             }
         }
         
-        len += prev.length() + (cnt == 1 ? 0 : (cnt+"").length());
+        len += prev.length() + (cnt == 1 ? 0 : (int)Math.log10(cnt)+1);
         return len;
     }
     String subs (int i, int c){
