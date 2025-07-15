acd0f9bed38d4119ada2a385afce6623a96fed85
hyezuu <147456219+hyezuu@users.noreply.github.com>
2025-07-15 11:32:40 +0900

    [Gold IV] Title: 사이클 게임, Time: 580 ms, Memory: 126084 KB -BaekjoonHub

--- 백준/Gold/20040. 사이클 게임/사이클 게임.java
+++ 백준/Gold/20040. 사이클 게임/사이클 게임.java
@@ -40,6 +40,11 @@ public class Main {
   static void union(int a, int b){
     a = find(a);
     b = find(b);
-    if(a != b) parent[b] = a;
+    
+    if(a < b) {
+      parent[b] = a;
+    }else {
+      parent[a] = b;
+    }
   }
 }
