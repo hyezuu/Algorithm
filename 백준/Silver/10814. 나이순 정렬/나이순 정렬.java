e4322d6f505b7afd5c20c64d590d5ffded9b6294
hyezuu <147456219+hyezuu@users.noreply.github.com>
2025-07-20 22:44:16 +0900

    [Silver V] Title: 나이순 정렬, Time: 428 ms, Memory: 41692 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
@@ -1,32 +1,36 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
 import java.util.StringTokenizer;
-import java.util.TreeMap;
 
 public class Main {
 
   public static void main(String[] args) throws Exception {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    Map<Integer, List<String>> map = new TreeMap<>();
     int N = Integer.parseInt(br.readLine());
+
+    StringBuilder[] members = new StringBuilder[201];
+
+    for(int i = 0; i < 201; i++) {
+      members[i] = new StringBuilder();
+    }
 
     for (int i = 0; i < N; i++) {
       StringTokenizer st = new StringTokenizer(br.readLine());
       int num = Integer.parseInt(st.nextToken());
-      String name = st.nextToken();
 
-      map.computeIfAbsent(num, k -> new ArrayList<>()).add(name);
+      members[num] = members[num]
+          .append(num)
+          .append(' ')
+          .append(st.nextToken())
+          .append("\n");
     }
 
     StringBuilder sb = new StringBuilder();
 
-    for (int key : map.keySet()) {
-      for(String name : map.get(key)) {
-        sb.append(key).append(" ").append(name).append("\n");
+    for(StringBuilder s : members) {
+      if(s.length() > 0) {
+        sb.append(s);
       }
     }
 
