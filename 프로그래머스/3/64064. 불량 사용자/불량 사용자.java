c595c917da6b7d7d8144e4253370b5457adde74c
hyezuu <147456219+hyezuu@users.noreply.github.com>
2025-08-02 11:17:51 +0900

    [level 3] Title: 불량 사용자, Time: 60.03 ms, Memory: 109 MB -BaekjoonHub

--- 프로그래머스/3/64064. 불량 사용자/불량 사용자.java
+++ 프로그래머스/3/64064. 불량 사용자/불량 사용자.java
@@ -2,17 +2,13 @@ import java.util.*;
 
 class Solution {
     static Set<Set<String>> result = new HashSet<>();
-    static boolean[] visited;
     static String[] bannedIds;
     static String[] userIds;
     
     public int solution(String[] userIds, String[] bannedIds) {
-        Solution.bannedIds = bannedIds;
-        Solution.userIds = userIds;
-        visited = new boolean[userIds.length];
-        
+        this.bannedIds = bannedIds;
+        this.userIds = userIds;
         dfs(0, new HashSet<>());
-        
         return result.size();
     }
 
@@ -23,15 +19,11 @@ class Solution {
         }
 
         for (int i = 0; i < userIds.length; i++) {
-            if (visited[i]) continue;
+            if (selected.contains(userIds[i])) continue;
             if (!checkFormat(bannedIds[bannedIdx], userIds[i])) continue;
 
-            visited[i] = true;
             selected.add(userIds[i]);
-
             dfs(bannedIdx + 1, selected);
-
-            visited[i] = false;
             selected.remove(userIds[i]);
         }
     }
