ccd26c2ed68d4cd5c4a1bd759ac862ea51e73384
hyezuu <147456219+hyezuu@users.noreply.github.com>
2025-08-11 11:03:58 +0900

    [Gold III] Title: 세 용액, Time: 228 ms, Memory: 17268 KB -BaekjoonHub

--- 백준/Gold/2473. 세 용액/세 용액.java
+++ 백준/Gold/2473. 세 용액/세 용액.java
@@ -29,42 +29,28 @@ public class Main {
     long[] answer = new long[3];
 
     for(int i=0; i<N-2; i++){
-      for(int j=i+1; j<N-1; j++){
-        if(i==j) continue;
+      int left = i + 1;
+      int right = N - 1;
 
-        long toFind = (solutions[i] + solutions[j])*-1;
-        long third = binarySearch(toFind, j+1);
+      while(left < right){
+        long sum = solutions[left] + solutions[right] + solutions[i];
 
-        long sum = (long)solutions[i] + solutions[j] + third;
         if(Math.abs(sum) < Math.abs(min)){
           min = sum;
-          answer[0] = solutions[i];
-          answer[1] = solutions[j];
-          answer[2] = third;
+          answer[0] = solutions[left];
+          answer[1] = solutions[right];
+          answer[2] = solutions[i];
+
           if(min == 0) break;
         }
+
+        if(sum < 0) left++;
+        else right--;
       }
+      if(min == 0) break;
     }
+
+    Arrays.sort(answer);
     System.out.print(answer[0] + " " + answer[1] + " " + answer[2]);
-  }
-
-  private static long binarySearch(long target, int start){
-    int end = N - 1;
-    if(start > end) return solutions[end];
-
-    long closest = solutions[start];
-
-    while(start <= end){
-      int mid = (start + end) / 2;
-
-      if(Math.abs(solutions[mid] - target) < Math.abs(closest - target)){
-        closest = solutions[mid];
-      }
-      if(solutions[mid] == target) return solutions[mid];
-      else if(solutions[mid] < target) start = mid + 1;
-      else end = mid - 1;
-    }
-
-    return closest;
   }
 }
